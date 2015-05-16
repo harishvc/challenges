49d00adebdf6ebb78bb147f69676a83f8a3cd47e
Harish Chakravarthy <harishvc@gmail.com>
2015-05-15 22:56:19 -0700

    binary tree traversal using DFA

--- tree-traversal.py
+++ tree-traversal.py
@@ -1,60 +1,209 @@
-#source: http://rosettacode.org/wiki/Tree_traversal#Python
+#BINARY TREES
+#1. Each node can no more than 2 child node
+#2. Value of the left child node is less than the value of the right child node
+#3. Value of the left and right child node is less than the parent node
 
-from collections import namedtuple
-from sys import stdout
- 
-Node = namedtuple('Node', 'data, left, right')
-tree = Node(1,
-            Node(2,
-                 Node(4,
-                      Node(7, None, None),
-                      None),
-                 Node(5, None, None)),
-            Node(3,
-                 Node(6,
-                      Node(8, None, None),
-                      Node(9, None, None)),
-                 None))
- 
-def printwithspace(i):
-    #stdout.write("%i " % i)
-    print "%i" % i,
+#Source: 
+#http://www.laurentluce.com/posts/binary-search-tree-library-in-python/
+#https://github.com/laurentluce/python-algorithms/blob/master/algorithms/binary_tree.py
 
-#root-left-right    
-def preorder(node, visitor = printwithspace):
-    if node is not None:
-        visitor(node.data)
-        #preorder(node.left, visitor)
-        #preorder(node.right, visitor)
- 
-def inorder(node, visitor = printwithspace):
-    if node is not None:
-        inorder(node.left, visitor)
-        visitor(node.data)
-        inorder(node.right, visitor)
- 
-def postorder(node, visitor = printwithspace):
-    if node is not None:
-        postorder(node.left, visitor)
-        postorder(node.right, visitor)
-        visitor(node.data)
- 
-def levelorder(node, more=None, visitor = printwithspace):
-    if node is not None:
-        if more is None:
-            more = []
-        more += [node.left, node.right]
-        visitor(node.data)
-    if more:    
-        levelorder(more[0], more[1:], visitor)
- 
-stdout.write('  preorder: ')
-preorder(tree)
+class Node:
+    """
+    Tree node: left and right child + data which can be any object
+    """
+    def __init__(self, data):
+        """
+        Node constructor
 
-#stdout.write('\n   inorder: ')
-#inorder(tree)
-#stdout.write('\n postorder: ')
-#postorder(tree)
-#stdout.write('\nlevelorder: ')
-#levelorder(tree)
-#stdout.write('\n')
+        @param data node data object
+        """
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        """
+        Insert new node with data
+
+        @param data node data object to insert
+        """
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def lookup(self, data, parent=None):
+        """
+        Lookup node containing data
+
+        @param data node data object to look up
+        @param parent node's parent
+        @returns node and node's parent if found or None, None
+        """
+        if data < self.data:
+            if self.left is None:
+                return None, None
+            return self.left.lookup(data, self)
+        elif data > self.data:
+            if self.right is None:
+                return None, None
+            return self.right.lookup(data, self)
+        else:
+            return self, parent
+
+    def delete(self, data):
+        """
+        Delete node containing data
+
+        @param data node's content to delete
+        """
+        # get node containing data
+        node, parent = self.lookup(data)
+        if node is not None:
+            children_count = node.children_count()
+            if children_count == 0:
+                # if node has no children, just remove it
+                if parent:
+                    if parent.left is node:
+                        parent.left = None
+                    else:
+                        parent.right = None
+                    del node
+                else:
+                    self.data = None
+            elif children_count == 1:
+                # if node has 1 child
+                # replace node by its child
+                if node.left:
+                    n = node.left
+                else:
+                    n = node.right
+                if parent:
+                    if parent.left is node:
+                        parent.left = n
+                    else:
+                        parent.right = n
+                    del node
+                else:
+                    self.left = n.left
+                    self.right = n.right
+                    self.data = n.data
+            else:
+                # if node has 2 children
+                # find its successor
+                parent = node
+                successor = node.right
+                while successor.left:
+                    parent = successor
+                    successor = successor.left
+                # replace node data by its successor data
+                node.data = successor.data
+                # fix successor's parent node child
+                if parent.left == successor:
+                    parent.left = successor.right
+                else:
+                    parent.right = successor.right
+
+    def compare_trees(self, node):
+        """
+        Compare 2 trees
+
+        @param node tree to compare
+        @returns True if the tree passed is identical to this tree
+        """
+        if node is None:
+            return False
+        if self.data != node.data:
+            return False
+        res = True
+        if self.left is None:
+            if node.left:
+                return False
+        else:
+            res = self.left.compare_trees(node.left)
+        if res is False:
+            return False
+        if self.right is None:
+            if node.right:
+                return False
+        else:
+            res = self.right.compare_trees(node.right)
+        return res
+
+    #root,left, right
+    def print_tree_preorder(self):
+        print self.data,
+        if self.left:
+            self.left.print_tree_preorder()
+        if self.right:
+            self.right.print_tree_preorder()
+    
+    #left, root, right    
+    def print_tree_inorder(self):
+        if self.left:
+            self.left.print_tree_inorder()
+        print self.data,
+        if self.right:
+            self.right.print_tree_inorder()
+            
+    #left, right, root    
+    def print_tree_postorder(self):
+        if self.left:
+            self.left.print_tree_postorder()
+        if self.right:
+            self.right.print_tree_postorder()
+        print self.data,
+    
+    def tree_data(self):
+        """
+        Generator to get the tree nodes data
+        """
+        # we use a stack to traverse the tree in a non-recursive way
+        stack = []
+        node = self
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else: # we are returning so we pop the node and we yield it
+                node = stack.pop()
+                yield node.data
+                node = node.right
+
+    def children_count(self):
+        """
+        Return the number of children
+
+        @returns number of children: 0, 1, 2
+        """
+        cnt = 0
+        if self.left:
+            cnt += 1
+        if self.right:
+            cnt += 1
+        return cnt
+
+
+#Get started ....
+root = Node(8)
+root.insert(3)
+root.insert(10)
+root.insert(1)
+
+#Time complexity is O(n) where n is number of nodes. Each node is visited exactly once.
+print "#preorder >>> ",
+root.print_tree_preorder()
+print "\n#inorder >>> ",
+root.print_tree_inorder()
+print "\n#postorder >>> ",
+root.print_tree_postorder()
+
