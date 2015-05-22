20ad8302e685d35962bf4aade7cf15af3e3751f8
Harish Chakravarthy <harishvc@gmail.com>
2015-05-21 22:09:59 -0700

    find factorial

---
+++ factorial.py
@@ -0,0 +1,32 @@
+#Question: Find factorial of n
+
+#Time Complexity: O(n^2)
+
+#Iterative solution
+def IFactorial(n):
+	result = 1
+	for x in range (1,n+1):
+		result *= x
+	return result
+
+#Recursive
+def RFactorial(n):
+	return 1 if (n < 1) else n * RFactorial(n-1)
+	
+print "### Find factorial ###"
+while True:
+	try:
+		x = int(raw_input("Enter number: ")) 
+		break
+	except:
+		print "Invalid number, please try again"
+
+#Solution 1: Iterative
+print ("%d!=%s" % (x,"{:,}".format(IFactorial(x))))
+
+#Solution 2: Recursive
+print ("%d!=%s" % (x,"{:,}".format(IFactorial(x))))
+
+#Solution 3: Recursive using lambda
+l3 = lambda x: 1 if (x < 1) else x * l3(x-1)
+print ("%d!=%s" % (x,"{:,}".format(l3(x))))
