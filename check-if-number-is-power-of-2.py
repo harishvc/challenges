d2fa2469f96b7b93b97aea25ee2e7cf1ca610378
Harish Chakravarthy <harishvc@gmail.com>
2015-06-02 21:01:19 -0700

    check if a number is power of 2 using bitwise operations

---
+++ check-if-number-is-power-of-2.py
@@ -0,0 +1,13 @@
+#Question: Given a number n, check whether the number is a power of 2
+
+#Observation: if (n & n-1 == 0) then the number is a power of 2
+
+#Solution
+input = [8,16,24,25,30,100]
+for n in input:
+    if (n & n-1 == 0):
+        print(n,":",True)
+    else:
+        print(n,":",False)
+
+
