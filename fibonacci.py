c229204d841a941a70cfea57fe042ec7834729fe
Harish Chakravarthy <harishvc@gmail.com>
2015-07-05 08:39:44 -0700

    Additional comments and add spacing

--- fibonacci.py
+++ fibonacci.py
@@ -11,16 +11,21 @@ References:
 1. https://www.mathsisfun.com/numbers/fibonacci-sequence.html
 2. http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/fibseries.html
 '''
-#Question: Find the Fibonacci series for a given range
+
+#Question: Find the Fibonacci sequence
 def fib(n):
     if n < 2:
         return n
     return fib(n-2) + fib(n-1) #recursive call
+
+#Range: 0-10
 start = 0  #start
 end =  10   #end
-print("range for fibonacci series:", range(start, end))
-print("fibonacci series:", list(map(fib, range(start,end+1))))
+print("range for Fibonacci series:", range(start, end))
+print("Fibonacci series:", list(map(fib, range(start,end+1))))
+
+#Range: 6-10
 start = 6  #start
 end =  10   #end
-print("range for fibonacci series:", range(start, end))
-print("fibonacci series:", list(map(fib, range(start,end+1))))
+print("range for Fibonacci series:", range(start, end))
+print("Fibonacci series:", list(map(fib, range(start,end+1))))
