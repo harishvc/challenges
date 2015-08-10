54aee134c07a8b61d3f7a5216ff37b10e1959d36
Harish Chakravarthy <harishvc@gmail.com>
2015-08-10 06:15:14 -0700

    update logic; add notes

--- find-maximum-gain.py
+++ find-maximum-gain.py
@@ -1,19 +1,23 @@
 '''
 Question: Given stock prices during a time period find the maximum gain that can be made 
 from one purchase followed by one sale of the stock
-'''
 
-'''
 Reference: 
   1. http://stackoverflow.com/questions/7086464/maximum-single-sell-profit
   2. http://keithschwarz.com/interesting/code/?dir=single-sell-profit
+
 Observation: Keep track of minimum price and gain
-Time Complexity: O(n)
-Space Conplexity: O(1) #constant
+
+Time Complexity: O(n) , Space Complexity: O(1) 
+
 Output: 
 [5, 10, 4, 6, 7] ->>>> Buy=5 Sell=10 Gain=5
 [5, 10, 4, 6, 12] ->>>> Buy=4 Sell=12 Gain=8
 [1, 2, 3, 4, 5] ->>>> Buy=1 Sell=5 Gain=4
+
+Dynamic Programming: (Bottom-up)
+Start with the first value of the input and keep building the solution for higher values
+
 '''
 
 inputs = [
@@ -28,13 +32,13 @@ for input in inputs:
 	min = input[0]
 	buy = input[0]  #optional
 	sell = input[0] #optional
-	for buyPrice in input:
+	for buyPrice in range (1, len(input)):
 		#Condition 1
-		if ( (buyPrice > min) and (buyPrice - min > gain)):
-			gain = buyPrice - min  # new gain 
-			sell = buyPrice  #sell price (optional)
+		if (input[buyPrice] - min > gain):
+			gain = input[buyPrice] - min  # new gain 
+			sell = input[buyPrice]  #sell price (optional)
 			buy = min        #buy price  (optional)
 		#Condition 2	
-		elif (buyPrice < min):
-			min = buyPrice #new min
+		elif (input[buyPrice] < min):
+			min = input[buyPrice] #new min
 	print("%s ->>>> Buy=%d Sell=%d Gain=%d" % (input,buy,sell,gain))
