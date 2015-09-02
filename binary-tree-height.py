63d140569a003024b07ef46d26f5049796c2cace
Harish Chakravarthy <harishvc@gmail.com>
2015-09-01 18:48:01 -0700

    find binary tree height

---
+++ binary-tree-height.py
@@ -0,0 +1,32 @@
+'''
+Question: Find the height of the binary tree
+
+Height is the number of nodes along the longest path from the root node down 
+to the farthest leaf node
+'''
+
+import sys
+sys.path.append("./mylib")
+import Tree
+
+def height(root):
+	if (root == None) :
+		return 0
+	x = height(root.left)
+	y = height(root.right)
+	z = max(x,y) + 1
+	return z
+
+
+root = Tree.BinaryTree(1)
+root.insertLeft(2)
+root.insertRight(3)
+root.getLeft().insertLeft(4)
+root.getLeft().insertRight(5)
+root.getRight().insertLeft(6)
+root.getRight().insertRight(7)
+root.getLeft().getLeft().insertLeft(8)
+root.getLeft().getRight().insertRight(9)
+root.getLeft().getRight().getRight().insertRight(10)
+root.getLeft().getRight().getRight().getRight().insertRight(11)
+print("height of node(%d) is %d" % (root.data,height(root)))
