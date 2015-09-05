3c90ef38e8c84007a01238dec2cb5fb37dbb553c
Harish Chakravarthy <harishvc@gmail.com>
2015-09-04 22:12:17 -0700

    change algorithm to compute in O(1) space complexity

--- binary-tree-leaves-nodes.py
+++ binary-tree-leaves-nodes.py
@@ -3,35 +3,41 @@
 import sys
 sys.path.append("./mylib")
 import Tree
-import queue
-
-def levelOrder(root):
-	if root is None: 
+        
+        
+fn = 0
+hn = 0
+leaf = 0
+#Modified inorder recursive traversal using global variables
+def inorderRecursive(root):
+	global fn,hn,leaf
+	if root is None:
 		return
-	nleaves = 0 #number of leaves
-	nfullnodes = 0 #number of full nodes
-	nhalfnodes = 0 #number of half nodes
-	q = queue.Queue()       
-	q.put(root)
-	n = None
-	while not q.empty():
-		n = q.get()  #dequeue FIFO
-		#Is this node a leaf?
-		if(n.left == None and n.right == None):
-			nleaves += 1
-		#Is this node a full node?
-		if(n.left and n.right):
-			nfullnodes += 1	
-		#Is this node a half node?
-		if ((n.left and not n.right) or (n.right and not n.left)):
-			nhalfnodes += 1
-		if n.left is not None:
-			#print("traversing left ..",n.left.getData())
-			q.put(n.left)
-		if n.right is not None:
-			#print("traversing right ..",n.right.getData())
-			q.put(n.right)  
-	return (nleaves,nfullnodes,nhalfnodes)		
+	if(root.getLeft() is not None and root.getRight() is not None):
+		fn += 1
+	elif (root.getLeft() is None and root.getRight() is None):
+		leaf += 1
+	else:
+		hn += 1		
+	if(root.getLeft() is not None):
+		inorderRecursive(root.left)
+	if(root.getRight() is not None):
+		inorderRecursive(root.right)
+
+#Modified inorder recursive traversal without using global variables
+def inorderRecursive2(root,fn,hn,leaf):
+	if(root.getLeft() is not None and root.getRight() is not None):
+		fn += 1
+	elif (root.getLeft() is None and root.getRight() is None):
+		leaf += 1
+	else:
+		hn += 1		
+	if(root.getLeft() is not None):
+		fn,hn,leaf = inorderRecursive2(root.left,fn,hn,leaf)
+	if(root.getRight() is not None):
+		fn,hn,leaf = inorderRecursive2(root.right,fn,hn,leaf)
+	return (fn,hn,leaf)
+
 
 root = Tree.BinaryTree(1)
 root.insertLeft(2)
@@ -44,7 +50,9 @@ root.getLeft().getLeft().insertLeft(8)
 root.getLeft().getRight().insertRight(9)
 root.getLeft().getRight().getRight().insertRight(10)
 root.getLeft().getRight().getRight().getRight().insertRight(11)
-nleaves,nfullnodes,nhalfnodes= levelOrder(root)
-print("#leaves = %d , #fullnodes = %d , #halfnodes = %d" % (nleaves,nfullnodes,nhalfnodes))
 
- 
+inorderRecursive(root)
+print("#leaves = %d , #fullnodes = %d , #halfnodes = %d" % (leaf,fn,hn))
+
+fn2,hn2,leaf2 =  inorderRecursive2(root,0,0,0)
+print("#leaves = %d , #fullnodes = %d , #halfnodes = %d" % (leaf2,fn2,hn2))
