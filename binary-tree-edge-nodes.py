e2fc1e2c5b11e319f18d1e4cad41fcd1ecc93148
Harish Chakravarthy <harishvc@gmail.com>
2015-09-05 10:40:34 -0700

    Update alogithm to find boundary nodes in O(n) complexity

--- binary-tree-edge-nodes.py
+++ binary-tree-edge-nodes.py
@@ -1,21 +1,39 @@
-#Question: Find all edge nodes in the binary tree
+'''
+Question: Find all edge (boundary) of the binary tree
+
+Algorithm:
+1. Print all left boundary nodes from top to bottom
+2. Print all leaf nodes from left to right
+3. Print all right boundary nodes from bottom to top
+
+Reference: http://stackoverflow.com/questions/30275735/to-print-the-boundary-of-binary-tree
+'''
 
 import sys
 sys.path.append("./mylib")
 import Tree
 
-def printEdgeNodes(root, pType, cType):
-   if root is None:
-       return
-   if pType == "root" or (pType == "left" and cType == "left") or (pType == "right" and cType == "right"):
-        print (root.data)
-   if root.left is None and root.right is None:
-       print (root.data)
-   if pType != cType and pType != "root":
-       cType = "invalid"
-   printEdgeNodes(root.left, cType, "left")
+#Modified from http://www.careercup.com/question?id=18065671
+#Time complexity: O(n)
+def TreeBoundry(node,isLeft,isRight):
+    #Left most node and leaf nodes
+    if(isLeft or isLeaf(node)):
+        print(node.data,end=' ')
+    #Next left node
+    if(node.getLeft() is not None): TreeBoundry(node.getLeft(), True, False)
+    #Next right node
+    if(node.getRight() is not None):TreeBoundry(node.getRight(), False ,True)
+    #Right most node
+    if(isRight and not isLeft and  not isLeaf(node)):
+        print(node.data,end=' ')
 
+def isLeaf(node):
+    if (node.getLeft() is None and  node.getRight() is None):
+        return True
+    else:
+        return False
 
+#Tree 1
 root = Tree.BinaryTree(1)
 root.insertLeft(2)
 root.insertRight(3)
@@ -27,4 +45,37 @@ root.getLeft().getLeft().insertLeft(8)
 root.getLeft().getRight().insertRight(9)
 root.getLeft().getRight().getRight().insertRight(10)
 root.getLeft().getRight().getRight().getRight().insertRight(11)
-printEdgeNodes(root, "root", "root")
+print("Tree 1: ", end="")
+TreeBoundry(root,True,True)
+print("")
+
+#Tree 2
+root2 = Tree.BinaryTree(1)
+root2.insertLeft(2)
+root2.insertRight(3)
+root2.getLeft().insertLeft(4)
+root2.getLeft().insertRight(5)
+root2.getRight().insertLeft(6)
+root2.getRight().insertRight(7)
+root2.getLeft().getRight().insertRight(8)
+print("Tree 2: ", end="")
+TreeBoundry(root2,True,True)
+print("")
+
+
+#Tree 3
+root3 = Tree.BinaryTree(1)
+root3.insertLeft(2)
+root3.insertRight(3)
+root3.getLeft().insertLeft(4)
+root3.getLeft().insertRight(5)
+root3.getRight().insertLeft(6)
+root3.getRight().insertRight(7)
+root3.getLeft().getLeft().insertLeft(8)
+root3.getLeft().getLeft().insertRight(9)
+root3.getRight().getLeft().insertLeft(10)
+root3.getRight().getLeft().insertRight(11)
+root3.getRight().getRight().insertRight(12)
+print("Tree 3: ", end="")
+TreeBoundry(root3,True,True)
+print("")
