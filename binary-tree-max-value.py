cf346066b31ae382f101c4e3f482146138985a9e
Harish Chakravarthy <harishvc@gmail.com>
2015-09-06 11:56:20 -0700

    simplify solution

--- binary-tree-max-value.py
+++ binary-tree-max-value.py
@@ -1,37 +1,36 @@
-#Question: Find the max value of the binary tree
+'''
+Question: Find the max value of the binary tree
+
+Algorithm:
+1. Recursively traverse the tree
+2. Recursive function take node and max value computed so far and returns new max value
+'''
 
 import sys
 sys.path.append("./mylib")
 import Tree
 
-#Find maximum value
-def FindMax(root):
-	global DefaultMax  #global variable since function is recursive   
-	if not root:   #Handle null node
-		return
-	if (root.data > DefaultMax):
-		DefaultMax = root.data
-	FindMax(root.right)
-	FindMax(root.left)	
+
+#Recursive traversal
+def MaxValue(root,maxvalue):
+	if (root.data >  maxvalue):
+		maxvalue = root.data
+	maxLeft = maxvalue
+	maxRight = maxvalue	
+	if (root.getLeft() is not None):
+		maxLeft  = MaxValue(root.getLeft(),maxvalue)
+	if (root.getRight() is not None):
+		maxRight = MaxValue(root.getRight(),maxvalue)
+	return max(maxLeft,maxRight)
 	
-def TreeSize(root):
-	if not root:
-		return 0
-	return TreeSize(root.left) + TreeSize(root.right) + 1 	
 
 #Initialize Binary Tree
 root = Tree.BinaryTree(1)
 root.insertLeft(2)
 root.insertRight(3)
-root.getLeft().insertLeft(4)
+root.getLeft().insertLeft(40)
 root.getLeft().insertRight(5)
 root.getRight().insertLeft(6)
-root.getRight().insertRight(7)
-result = []
-#
-#
-#
-DefaultMax = float("-infinity") #Storing max in a global variable since the function is recursive
-FindMax(root)
-print("Maximum value in the binary tree =",DefaultMax)
- 
+root.getRight().insertRight(70)
+
+print("Maximum value =",MaxValue(root,root.data))
