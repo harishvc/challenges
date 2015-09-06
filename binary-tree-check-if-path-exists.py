ee610b1c158e970add571be716f58224e7b1f32d
Harish Chakravarthy <harishvc@gmail.com>
2015-09-06 11:59:11 -0700

    simplify algorithm

--- binary-tree-check-if-path-exists.py
+++ binary-tree-check-if-path-exists.py
@@ -4,21 +4,16 @@ import sys
 sys.path.append("./mylib")
 import Tree
         
-def pathFinder(root, val, path, paths):
-	if not root:
-		return False    
-	if not root.left and not root.right:
-		if root.data == val:
-			path.append(root.data)
-			paths.append(path)
-			return True
-		else:
-			return False
-	left = pathFinder(root.left, val - root.data, path + [root.data], paths)
-	right = pathFinder(root.right, val - root.data, path + [root.data], paths)  
-	return left or right
-     	
-######## Get Started!!!!!
+def ValidPathSum(root,currentsum):
+    global Complete
+    if (root.getLeft() is None and root.getRight() is None and not Complete):
+        if (currentsum == PATHSUM):
+            Complete = True
+    if(root.getLeft() is not None and not Complete):
+        ValidPathSum(root.getLeft(),currentsum+root.getLeft().data)
+    if(root.getRight() is not None and not Complete):
+        ValidPathSum(root.getRight(),currentsum+root.getRight().data)
+         	
 root = Tree.BinaryTree(1)
 root.insertLeft(2)
 root.insertRight(3)
@@ -30,6 +25,8 @@ root.getLeft().getLeft().insertLeft(8)
 root.getLeft().getRight().insertRight(9)
 root.getLeft().getRight().getRight().insertRight(10)
 root.getLeft().getRight().getRight().getRight().insertRight(11)
-print ("Is 10 valid sum?",pathFinder(root,10,[],[]) )
-print ("Is 15 valid sum?",pathFinder(root,15,[],[]) )
-print ("Is 9 valid sum?",pathFinder(root,9,[],[]) )
+
+Complete = False #GLOBAL
+PATHSUM = 10     #GLOBAL
+ValidPathSum(root, root.data)
+print ("Is %d valid sum? %s" % (PATHSUM,Complete))
