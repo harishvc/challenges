8059d0041e0c2e52eb8e85cd4799073d68fcbc82
Harish Chakravarthy <harishvc@gmail.com>
2015-09-08 06:31:08 -0700

    clean up;add comments;easy to scan and follow along

--- binary-search-tree-check.py
+++ binary-search-tree-check.py
@@ -1,10 +1,10 @@
-#Question: Check if a BST is valid
+'''
+Question: Check if a BST is valid
 
-#Properties to check
-#max value of left sub-tree is smaller than current node data
-#min value of right sub tree is greater than current node data
-
-#Source: https://github.com/careermonk/DataStructureAndAlgorithmicThinkingWithPython/blob/master/src/chapter06trees/IsBST.py
+Properties to check:
+1. max value of left sub-tree is smaller than current node data
+2. min value of right sub tree is greater than current node data
+'''
 
 class BSTNode:
     def __init__(root, data):
@@ -26,64 +26,23 @@ def insertNode(root, node):
                 root.right = node
             else:
                 insertNode(root.right, node)
-
-
-def inorderIterative(root, result):
-    if not root:
-        return
-    stack = []
-    node = root
-    while stack or node:
-        if node:
-            stack.append(node)
-            node = node.left
-        else:
-            node = stack.pop()
-            result.append(node.data)
-            node = node.right
-            
-def FindMin(root):
-    """ return the minimum node in the current tree and its parent """
-    # we use an ugly trick: the parent node is passed in as an argument
-    # so that eventually when the leftmost child is reached, the 
-    # call can return both the parent to the successor and the successor
-    if root.left:
-        return FindMin(root.left)
-    else:
-        return root
-
-#Right most node that does not have a right child        
-def FindMax(root):
-    if root.right:
-        return FindMax(root.right)
-    else:
-        return root
     
-#Source: https://github.com/careermonk/DataStructureAndAlgorithmicThinkingWithPython/blob/master/src/chapter06trees/IsBST.py
-#max value of left sub-tree is smaller than current node data
-#min value of right sub tree is greater than current node data
-# Returns true if a binary tree is a binary search tree 
-def IsBST3(root):
+def ValidBST(root):
+    #Condition 1: return true if no node found
     if root == None:
         return True
     
-    # false if the max of the left is > than root 
+    #Condition 2: false if the max of the left is > than root 
     elif(root.left != None and (root.left.data > root.data)):
        return False
 
-    # false if the min of the right is <= than root 
+    #Condition 3: false if the min of the right is <= than root 
     elif(root.right != None and (root.right.data < root.data)):
        return False
-
-    # false if, recursively, the left or right is not a BST 
-    #if(not IsBST3(root.left) or not IsBST3(root.right)): 
-       #return False
-
-    
-    # passing all that, it's a BST 
-    #return True
+   
+    #Condition 4: check left and right node
     else:
-        return (IsBST3(root.left) and IsBST3(root.right))
+        return (ValidBST(root.left) and ValidBST(root.right))
 
 #Create BST
 root = BSTNode(4)
@@ -91,7 +50,5 @@ insertNode(root, BSTNode(3))
 insertNode(root, BSTNode(2))
 insertNode(root, BSTNode(1))
 insertNode(root, BSTNode(5))
-result = []
-inorderIterative(root, result)
-result = IsBST3(root)
-print ("BST valid?", result)
+#Valid BST?
+print ("BST valid?", ValidBST(root))
