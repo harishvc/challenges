8c1bd600c10d9a223268485d97a6f0a89443fbf4
Harish Chakravarthy <harishvc@gmail.com>
2015-09-12 23:30:56 -0700

    clean up

--- binary-tree-max-depth.py
+++ binary-tree-max-depth.py
@@ -1,33 +1,36 @@
 #Question: Find max depth of binary tree
+#Depth of a node is the length of the path (#nodes) from the root to the node
 
 import sys
 sys.path.append("./mylib")
 import Tree
-
-#Source: http://jelices.blogspot.com/2014/05/leetcode-python-maximum-depth-of-binary.html
-def MaxDepthIterative(root):
-	if root == None:
-		return 0
-	nodeStack = [root]  #Store nodes in array
-	depthStack = [1]    #Store depth of each level in array
-	maxDepth = 0        #Default max depth
-	while nodeStack:	
-		node = nodeStack.pop()
-		depth = depthStack.pop()
-		maxDepth = max (maxDepth, depth)
-		if node.left != None:
-		 	nodeStack.append(node.left) #append left node
-		 	depthStack.append(depth+1)  #append depth of left node
-		if node.right != None:
-			nodeStack.append(node.right) #append right node
-			depthStack.append(depth+1)   #append right node depth
-	return maxDepth
+from heapq_showtree import show_tree
  
 def MaxDepthRecursive(root):
 	if (root == None):
 		return 0 
 	return max(MaxDepthRecursive(root.right),MaxDepthRecursive(root.left)) + 1  	
 
+#Source: http://jelices.blogspot.com/2014/05/leetcode-python-maximum-depth-of-binary.html
+def MaxDepth(root):
+	maxDepth = 0
+	if root is None:
+		return maxDepth
+	nodes = []
+	depth = []
+	nodes.append(root) 
+	depth.append(1) #root node at depth 1
+	while nodes:  
+		newNode =  nodes.pop()
+		newDepth = depth.pop()
+		maxDepth = max(maxDepth,newDepth)
+		if(newNode.left is not None):
+			nodes.append(newNode.left)   #Add new node to stack
+			depth.append(newDepth + 1)   #Add height of new node in stack
+		if(newNode.right is not None):
+			nodes.append(newNode.right)  #Add new node to stack
+			depth.append(newDepth + 1)   #Add height of new node in stack
+	return maxDepth
 
 root = Tree.BinaryTree(5)
 root.insertLeft(9)
@@ -40,4 +43,7 @@ root.getLeft().getLeft().insertLeft(8)
 root.getLeft().getRight().insertRight(9)
 root.getLeft().getRight().getRight().insertRight(10)
 root.getLeft().getRight().getRight().getRight().insertRight(11)
-print("Max depth of the binary tree (iterative) =",MaxDepthIterative(root))
+
+#Find max depth
+print("Max depth of the binary tree (iterative) =",MaxDepth(root))
+print("Max depth of the binary tree (recursive) =",MaxDepthRecursive(root))
