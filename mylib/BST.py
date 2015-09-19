1ef43339673b95da053db0ee19cdd740acf5c897
Harish Chakravarthy <harishvc@gmail.com>
2015-09-18 22:39:47 -0700

    BST Library

---
+++ mylib/BST.py
@@ -0,0 +1,23 @@
+#Binary Search Tree Library
+
+class BSTNode:
+    def __init__(root, data):
+        root.left = None
+        root.right = None
+        root.data = data
+
+def insertNode(root, node):
+    if root is None:
+        root = node
+    else:
+        if root.data > node.data:
+            if root.left == None:
+                root.left = node
+            else:
+                insertNode(root.left, node)
+        else:
+            if root.right == None:
+                root.right = node
+            else:
+                insertNode(root.right, node)
+
