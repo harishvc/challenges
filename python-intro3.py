2eac941174581cd98627ae4696cd18c3f738aedf
Harish Chakravarthy <harishvc@gmail.com>
2015-09-28 22:07:51 -0700

    fun with list :speaker: pencil2:

--- python-intro3.py
+++ python-intro3.py
@@ -1,16 +1,30 @@
 ####LIST INTRO
 x = [1, 2, 3, 4]
-print(x[3])
-print(x[-1])
-print(x[-3])
-print(x[1:3])
-print(x[:2])
-print(x[1:])
-print(x[-2:3])
-print(x[::2])
-print(x[::-1]) #reverse
-print(x[-1:0:-1])
+print(x[3])        #index position 3, 4th element
+print(x[-1])       #last index
+print(x[-3])       #3rd index from the end
+print(x[1:3])      #elements in index 1 & 2
+print(x[:2])       #elements in index 0 & 1
+print(x[1:])       #elements in index 1 to end of list
+print(x[-2:4])     #elements in between 2nd index from the end and 3rd index from start
+print(x[::2])      #every other element is skipped
+print(x[:-2])      #elements in index 0,1
+print(x[-2:])      #elements in index 2,3
+print(x[::-1])     #reverse
+print(x[-1:0:-1])  #elements in index position 3 (last, -1) and index position 1 going left
+x.insert(0,0)      #insert 0 in index position 0
+x.append(5)        #append 5 at the end
+del x[0:1]         #delete elements in index 0
+del x[:]           #delete ALL elements inside x
 
+###LIST & INSERT
+a = []             # []
+a.insert(0,1)      # [1]
+a.insert(0,2)      # [2,1]
+a.insert(0,3)      # [3,2,1]
+y = []             # []
+y.insert(5,100)    # [100]
+print(len(y))      # 1
 
 ####Sort List
 lucky = ['22', '11', '31']
