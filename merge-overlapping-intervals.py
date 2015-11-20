f24d646ac3cb01ca64278cbf8893bbb33eeb598c
Harish Chakravarthy <harishvc@gmail.com>
2015-11-19 22:18:22 -0800

    update notes :notes:

--- merge-overlapping-intervals.py
+++ merge-overlapping-intervals.py
@@ -11,19 +11,20 @@ def MergeInterval(input):
     #print("sorted input .....", input)
     
     #Step2: Iterate through the sorted list
+    #Initialize with first entry
     result = [[input[0][0],input[0][1]]]
     end = input[0][1] 
     for x in range(1,len(input)):
-        #New start is less than or equal to last seen end - MERGE!
+        #New start is less than or equal to end
         if (input[x][0] <= end):
-            #New end is larger than last seen end 
+            #New end is larger than end 
             if (input[x][1] > result[-1][1]):
-                result[-1][1] = input[x][1]
-                end = input[x][1]  
+                result[-1][1] = input[x][1] #Merge!
+                end = input[x][1]  #new end
         #New start is greater than last seen end
         else:
-            result.append([input[x][0],input[x][1]])
-            end = input[x][1]  
+            result.append([input[x][0],input[x][1]]) #new time interval
+            end = input[x][1]  #new end
     return result
 
 
