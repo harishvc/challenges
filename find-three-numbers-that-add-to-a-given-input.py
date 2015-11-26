02ebdc53319a3446895194c9923f872a8d73f8d8
Harish Chakravarthy <harishvc@gmail.com>
2015-11-25 20:17:44 -0800

    optimized code and works! :thumbsup:

--- find-three-numbers-that-add-to-a-given-input.py
+++ find-three-numbers-that-add-to-a-given-input.py
@@ -6,9 +6,8 @@ Question: Given a list and sum. Find 3 numbers in the list that add up to the gi
 Solution1: Brut-force
 1. Run 3 loops to find sum
 2. Time complexity: O(n^3)
-'''
 
-'''
+
 Solution 2:
 Reference: http://stackoverflow.com/questions/2070359/finding-three-elements-in-an-array-whose-sum-is-closest-to-an-given-number#
 
@@ -23,6 +22,7 @@ Step 3: Iterate throught the list such that a[i]+a[j]+a[k] = 3sum or while k>= j
  
 Time complexity: nlogn + O(n^2)
 '''
+#Finds the first match and does not handle duplicates - inaccurate!
 def findNumbers(input,sum):
     input.sort()
     size = len(input)
@@ -40,11 +40,79 @@ def findNumbers(input,sum):
             else:
                 j += 1
     print("None")
-    
-input = [2, 3, -1, 1]
-sum = 4
-print("input ===>", input)
-print("Triplets adding to %d = " % (sum),end="")
-findNumbers(input, sum)
 
+
+'''
+Questions to ask:
+a. Is the input sorted? No
+b. Are there duplicates? Yes
+c. Can the triplets get printed as they are found? No  Return list with triplets
+
+Solution 3: Modified solution #2 to handle duplicate values.
+Time complexity: nlogn + O(n^2) , n is #unique values
+
+Reference:
+http://www.programcreek.com/2012/12/leetcode-3sum/
+'''
+#Handle duplicate values
+def findNextb(b,c,input):
+    while(b < c and input[b] == input[b-1]):
+        b +=1
+    return b
+
+#Handle duplicate values
+def findNextc(b,c,input):
+    while(b < c and input[c] == input[c+1]):
+        c -=1
+    return c
+   
+def find3Numbers(input,sum):
+    result = []
+    length = len(input)
+    #Validation
+    if (length <= 2):
+        return result
+    #Step 1: Sort the input
+    input.sort()
+    #print("sorted .....", input)
+    #Step 2: Iterate the input with 2 pointers
+    for i in range(length-2):
+        #Handle duplicates
+        if (i == 0 or (input[i] > input[i-1])):
+            a = i
+            b =  i + 1
+            c = length -1
+            #a + b + c = sum
+            #b + c  = sum - a
+            wanted = sum - input[a]      
+            #print("a=%d b=%d c=%d wanted=%d" %(a,b,c,wanted))
+            while(b < c):
+                #print("checking .....",input[b], input[c])
+                bc = input[b] + input[c]
+                #Match
+                if (bc == wanted):
+                    result.append([input[a],input[b],input[c]])
+                    b += 1
+                    c -= 1
+                    #Duplicate values
+                    b = findNextb(b,c,input)
+                    c = findNextc(b,c,input)
+                #sum of b+c is less, find next greater value
+                elif( bc < wanted):
+                    b += 1
+                    #Duplicate values
+                    b = findNextb(b,c,input)
+                #sum of b+c is more, find next lesser value    
+                else:
+                    c -=1
+                    #Duplicate values
+                    c = findNextc(b,c,input)
+    return result
+   
+input = [-1, 0, 1, 2, -1, -4]
+sum = 0
+print("input ===>", input)
+print("Listing triplets adding to %d" % (sum))
+result = find3Numbers(input, sum)
+print(result)
     
