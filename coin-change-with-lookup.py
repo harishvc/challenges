ed685cb16157327fa0053c4c77380b6a2f528d4d
Harish Chakravarthy <harishvc@gmail.com>
2015-12-02 20:50:01 -0800

    Fix issues;add notes :notes: thumbsup:

--- coin-change-with-lookup.py
+++ coin-change-with-lookup.py
@@ -1,63 +1,60 @@
 '''
-Find total #ways to reach a total using given denominations with lookup
+Question: Given denominations and a total, find total #ways to reach a total using given denominations. 
 
-TODO:
-1. Handle [2,3], [3,2]
-2. Calculate [1,1]
-
+Solution:
+Inspired by the Python solution from @leif posted at http://stackoverflow.com/questions/1106929/find-all-combinations-of-coins-when-given-some-dollar-value
+New solution created with lookup
 '''
 
-denominations = [1,2,3,4]
 
 def MyLength(self):
     return len(self)
 
-
-def Lookup(map,key,values):
-    if(key in map.keys()):
-        t1 = map[key]
-        if values not in t1:    
-            t1.append(values)
-            map[key] = t1
-        #else:
-            #print("ignoring .....", values)
+def LocalLookup(mapping,key,value):
+    value.sort() #IMPORTANT! , resolves [2,3][3,2]
+    if(key in mapping.keys()):
+        if value not in mapping[key]:
+            mapping[key].append(value)
     else:
-        map[key] = [values]
-
-
-def Combinations(total,prefix,result,map):
-    global denominations
+        #new
+        mapping[key] = [value]  #[] don't unpack
+        
+def Combinations(total,prefix,result,mapping):
     for i in range(len(denominations)):
         if (denominations[i] <= total):
-            #multiples
-            if ( total % denominations[i] == 0):
-                t1 = [denominations[i] for x in range(int(total/denominations[i]))]
-                Lookup(map,total,t1)
-                if(len(prefix) > 0):
-                    t2 = [prefix[-1]] + t1
-                    Lookup(map,sum(t2),t2)
-                newcombination = prefix + t1
-                Lookup(map,sum(newcombination), newcombination)
-                if newcombination not in result:
-                    result += [newcombination]
+            #multiple
+            if total % denominations[i]  == 0:
+                tmp1 = [denominations[i] for x in range(0,int(total/denominations[i]))]
+                #Add to lookup
+                LocalLookup(mapping,total,tmp1)
+                tmp2 = prefix + tmp1
+                tmp2.sort() #IMPORTANT! , resolves [2,3][3,2]
+                if tmp2 not in result:
+                    result.append(tmp2)
+            #combinations
             else:
-                #find combinations
-                new_total = total - denominations[i]
-                new_prefix = prefix +[denominations[i]]
-                if(len(new_prefix) > 0):
-                    Lookup(map,sum(new_prefix), new_prefix)
-                if new_total in map.keys():
-                    for i in range(len(map[new_total])):
-                        result.append(new_prefix + map[new_total][i])
-                else:    
-                    #missing 2 >> [1,1]
-                    result = Combinations(new_total,new_prefix,result,map)
+                new_total = total - denominations[i]    
+                new_prefix = prefix + [denominations[i]] #send prefix as list
+                #Add to lookup
+                if (len(new_prefix) > 1):
+                    LocalLookup(mapping,sum(new_prefix),new_prefix)
+                #Check lookup before recursion
+                if (new_total in mapping.keys()):
+                    tmp3 = mapping[new_total]
+                    for i in range(len(tmp3)):
+                        tmp4 = new_prefix + tmp3[i]
+                        result.append(tmp4)
+                else:
+                    result = Combinations(new_total,new_prefix,result,mapping)
+        else:
+            return result
     return result
 
+denominations = [1,2,3,4,5]
 total = 5
 result = []
-map = {}
-Combinations(total,[],result,map)
+mapping = {}
+Combinations(total,[],result,mapping)
 print("Total=%d denominations=%s" % (total,denominations))
 print("# combinations",len(result))
 print("All possible combinations >>>",result)
@@ -65,5 +62,5 @@ result.sort(key=MyLength)
 print("Most optimal solution:",result[0])
 
 #Print map
-#for key in map:
-#    print(key, ">>>", map[key])
+#for key in mapping:
+#    print(key, ">>>", mapping[key])
