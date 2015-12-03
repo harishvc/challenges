787209e9512d273fa7ee464b4d1b746304ed1b8f
Harish Chakravarthy <harishvc@gmail.com>
2015-12-02 20:52:58 -0800

    Add notes

--- binary-tree-root-to-leaf-paths-recursive.py
+++ binary-tree-root-to-leaf-paths-recursive.py
@@ -5,21 +5,23 @@ Question: Print all paths & path sum from root to leaf (recursive)
 import sys
 sys.path.append("./mylib")
 import Tree
-
-def SumRoot2LeafPathsRecursive(node,sum,path):
-     if(node is None):
-         return 0
-     sum = sum + node.data  #sum of path
-     path.append(node.data) #path
-     #leaf
-     if(node.getLeft() is None and node.getRight() is None):
-         print("path=%s sum=%d" %(path,sum))
-         path.pop()
-         return sum-node.data 
-     SumRoot2LeafPathsRecursive(node.getLeft(),sum,path)
-     SumRoot2LeafPathsRecursive(node.getRight(),sum,path)
-     path.pop() #Important
  
+
+#Print all root to leaf path and sum
+def Root2Leaf(node,path,sum):
+    if (node is None):
+        return
+    sum += node.data
+    path.append(node.data)    
+    if(node.getRight() is None and node.getLeft() is None):
+        print(path, sum)
+        path.pop()
+        return
+    Root2Leaf(node.getLeft(),path,sum)
+    Root2Leaf(node.getRight(),path,sum)
+    path.pop() #pop non-leaf nodes while leaving
+
+
      
 #Build binary tree 
 root = Tree.BinaryTree(1)
@@ -33,5 +35,6 @@ root.getRight().insertRight(7)
 
 path = []
 print("root to leaf path & sum:")
-SumRoot2LeafPathsRecursive(root,0,path)
+#SumRoot2LeafPathsRecursive(root,0,path)
+Root2Leaf(root,path,0)
 assert len(path) == 0, "Houston we have a problem!"
