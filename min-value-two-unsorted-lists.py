22cc9c4b197043da5cebab9a6372f38550238855
Harish Chakravarthy <harishvc@gmail.com>
2016-01-06 18:30:09 -0800

    simplify code :boom:

--- min-value-two-unsorted-lists.py
+++ min-value-two-unsorted-lists.py
@@ -6,7 +6,9 @@ a = [6,3,4,8]
 b = [9,4,10,3]
 
 #Step 1: Iterate through both the list and find the intersection
-c = [val for val in a if val in b]
+#c = [val for val in a if val in b]
+#simple generator!
+c = [x for x in a and b]
 
 #Step 2: Iterate through the intersection to find the min
 min = c[0]
