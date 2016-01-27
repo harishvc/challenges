5f15bd65dd26625629bbc038a5abbd618d607718
Harish Chakravarthy <harishvc@gmail.com>
2016-01-26 21:17:52 -0800

    simplify;add notes :notes:

--- find-maximum-gain.py
+++ find-maximum-gain.py
@@ -6,8 +6,17 @@ Reference:
   1. http://stackoverflow.com/questions/7086464/maximum-single-sell-profit
   2. http://keithschwarz.com/interesting/code/?dir=single-sell-profit
 
-Observation: Keep track of minimum price and gain
+Observation:
+Each data point we need to make a decision - buy or sell?
+1. Time to Sell? 
+   a. New data point > buy
+   b. Profit from new data point is > profit
+       b1. profit = sell-buy
+       b2. profit = sell-maybebuy  
+2. Time to buy?
+   a. if new data point is < buy, perhaps "maybebuy" since we don't know about the following data points    
 
+Complexity:
 Time Complexity: O(n) , Space Complexity: O(1) 
 
 Output: 
@@ -17,28 +26,26 @@ Output:
 
 Dynamic Programming: (Bottom-up)
 Start with the first value of the input and keep building the solution for higher values
-
 '''
 
-inputs = [
-		  [5,10,4,6,7],
-		  [5,10,4,6,12],
-		  [1,2,3,4,5]
-		 ]
+input = [5,10,4,6,7,2,7]
+#input = [1,2,3,4,5]
+#input = [5,1]
 
+buy = input[0]
+sell = None
+profit = None
+maybenewbuy = None
 
-for input in inputs:
-	gain = 0
-	min = input[0]
-	buy = input[0]  #optional
-	sell = input[0] #optional
-	for buyPrice in range (1, len(input)):
-		#Condition 1
-		if (input[buyPrice] - min > gain):
-			gain = input[buyPrice] - min  # new gain 
-			sell = input[buyPrice]  #sell price (optional)
-			buy = min        #buy price  (optional)
-		#Condition 2	
-		elif (input[buyPrice] < min):
-			min = input[buyPrice] #new min
-	print("%s ->>>> Buy=%d Sell=%d Gain=%d" % (input,buy,sell,gain))
+for x in range(1,len(input)):
+	#Scenario 1: Sell
+	if(input[x] > buy or (profit is not None and profit < input[x]-buy) or (maybenewbuy is not None and profit < input[x]-maybenewbuy)):
+		sell = input[x]
+		if (maybenewbuy is not None):
+			buy = maybenewbuy
+			maybenewbuy = None
+		profit = sell - buy
+	#Scenario 2: Buy
+	elif(input[x] < buy):
+		maybenewbuy = input[x]
+print("Buy=%d Sell=%d Profit=%d" % (buy,sell,profit))
