dabc13ed0f925c27f128b67d45330a295c6a5e44
Harish Chakravarthy <harishvc@gmail.com>
2016-02-05 19:21:13 -0800

    Clean up; Add Notes :notes:

--- sort-quicksort.py
+++ sort-quicksort.py
@@ -1,37 +1,65 @@
 #Quick Sort
 
-#source:https://github.com/careermonk/DataStructureAndAlgorithmicThinkingWithPython/blob/master/src/chapter10sorting/QuickSort.py
-#Notes
-# 1. Example of divide and conquer
-# 2. Picks an element in the array to be a pivot
-# 3. Average O(n* log n)
-# 4. Worse case O(n^2)
- 
-import random
-#low and high are index position of list elements
-#pivot is a value
-def QuickSort(A, low, high):
-    if low < high:
-        pivot = Partition(A, low, high)
-        QuickSort(A, low, pivot - 1)
-        QuickSort(A, pivot + 1, high)
- 
-def Partition(A, low, high) :
-    pivot = low + random.randrange(high - low + 1)
-    swap(A, pivot, high)
-    for i in range(low, high):
-        if A[i] <= A[high]:
-            swap(A, i, low)
-            low += 1
-    swap(A, low, high)
-    return low
- 
-def swap(A, x, y):
-    A[x],A[y] = A[y],A[x]
+'''
+ALGORITHM:
+1. Quick sort uses divide and conquer
+2. Quick sort first selects a pivot value to split the list. 
+3. There are many ways to select the pivot value - first , last, random
+4. Index position of the pivot value is commonly called the split point
+5. Split point is used to divide the list for subsequent calls to the quick sort
 
-A = [534, 246, 933, 127, 277, 321, 454, 565, 220]
-random.shuffle(A)
-print("Input >>", A)
-QuickSort(A, 0, len(A) - 1)
-print("Quick Sort: ", A)
-assert A ==  [127, 220, 246, 277, 321, 454, 534, 565, 933], "ERROR"
+NOTES:
+1. Divide and conquer
+2. Inplace algorithm 
+3. NO need for extra memory
+4. NOT STABLE - does not retain the order of same values
+5. When list size becomes 16-20, quick sort becomes insertion sort
+6. DEFAULT system sort
+7. Time complexity:
+   - Each of the n items needs to check against the pivot value - O(n)
+   - There are log n divisions after the split point is found
+   - average: O(nlogn)
+   - worst case:  O(n^2)
+'''
+
+#http://interactivepython.org/runestone/static/pythonds/SortSearch/TheQuickSort.html
+import random
+
+def quickSort(alist):
+   quickSortHelper(alist,0,len(alist)-1)
+
+def quickSortHelper(alist,first,last):
+   if first<last:
+       splitpoint = partition(alist,first,last)
+       quickSortHelper(alist,first,splitpoint-1)
+       quickSortHelper(alist,splitpoint+1,last)
+
+
+def partition(alist,first,last):
+   pivotvalue = alist[first] #first value is pivot, many value to select pivot!
+   leftmark = first+1
+   rightmark = last
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+           leftmark = leftmark + 1
+
+       while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+       	   alist[leftmark],alist[rightmark] = alist[rightmark],alist[leftmark]
+
+   alist[first],alist[rightmark] = alist[rightmark],alist[first]        
+   return rightmark
+
+
+alist = [54,26,93,17,77,31,44,55,20]
+random.shuffle(alist)
+print("input >>>", alist)
+quickSort(alist)
+print("quick sort >>>", alist)
+assert alist == [17, 20, 26, 31, 44, 54, 55, 77, 93], "Quick sort error!"
