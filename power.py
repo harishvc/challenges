a4bd90c4d94172310511c06e99bb9ddbb03a65f1
Harish Chakravarthy <harishvc@gmail.com>
2016-02-20 18:33:29 -0800

    bit operation to compute n^k

--- power.py
+++ power.py
@@ -11,6 +11,8 @@
 #5. Proof:
 #    5.1: 2^15=32,768    
 #6. Code:
+
+#Solution 1
 def IPower(x,n):
     result = 1
     for i in range(n):
@@ -28,3 +30,30 @@ while True:
 t = IPower(x,n/2) #Divide and conquer
 result = t * t if (n % 2 == 0) else x*t*t #least significant bit in the exponent 
 print ("%d^%d=%s" % (x,n,"{:,}".format(result)))
+
+#Solution 2
+#Shifting bits
+#http://stackoverflow.com/questions/2198138/calculating-powers-e-g-211-quickly?lq=1
+def intpow(base, exp):
+        if exp == 0:
+                return 1
+        elif exp == 1:
+                return base
+        elif (exp & 1) != 0:
+                print("%d*intpow(%d*%d,%d//2)" %(base,base,base,exp))
+                return base * intpow(base * base, exp // 2)
+        else:
+                print("intpow(%d*%d,%d//2)" %(base,base,exp))
+                return intpow(base * base, exp // 2)
+
+base = float(2)
+exp = 13
+negative=False
+if(exp < 0):
+        negative = True
+        exp = abs(exp)
+result = intpow(base,exp)
+if(negative):
+        print(1/result)
+else:
+        print(result)
