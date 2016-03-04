b768af3b849a4e844b8826fb590ec0ad46ba9245
Harish Chakravarthy <harishvc@gmail.com>
2016-03-03 18:41:36 -0800

    Kahn alogorithm :thumbsup:

--- README.md
+++ README.md
@@ -262,8 +262,10 @@ Collection of interesting questions and solutions that involve data structures, 
    2.1 [DFS traversal] (https://github.com/harishvc/challenges/blob/master/graph-traversal-dfs.py)    
    2.2 [Modify DFS to keep track vertex state (white, black & gray)] (https://github.com/harishvc/challenges/blob/master/graph-find-path-between-nodes.py) :thumbsup: :notes:  
    2.3 [BFS traversal] (https://github.com/harishvc/challenges/blob/master/graph-traversal-bfs.py)  :thumbsup: :boom: 
-3. [Topological sort introduction](https://github.com/harishvc/challenges/blob/master/graph-topological-sort.py)
-4. [Topological sort using DFS](https://github.com/harishvc/challenges/blob/master/graph-topological-sort-using-DFS.py) :bulb: :thumbsup: :notes: :rocket:
+3. [Topological sort introduction]
+    3.1 [Topological sort (given edges)] (https://github.com/harishvc/challenges/blob/master/graph-topological-sort.py)   
+    3.2 [Topological sort using Kahn's algorithm (given adjacency list)] (https://github.com/harishvc/challenges/blob/master/graph-topological-sort-kahn.py) :notes: :rocket:  
+    3.3 [Topological sort using DFS](https://github.com/harishvc/challenges/blob/master/graph-topological-sort-using-DFS.py) :bulb: :thumbsup: :notes: :rocket:  
 5. [In a directed graph given two nodes find out whether if a path exists](https://github.com/harishvc/challenges/blob/master/graph-check-if-path-exists.py)
 
 
---
+++ graph-topological-sort-kahn.py
@@ -0,0 +1,63 @@
+'''
+Questions: 
+1. Check if a directed graph is acyclic
+2. Sort the vertex (topological sort)
+
+Directed Acyclic Graphs (DAG) have the following properties
+1. Their edges show direction  
+2. They don't have cycles
+
+Topological sort is ordering of the vertex in a directed acyclic graph (DAG), 
+such that if there is a edge from vertex u to vertex v, 
+then v appears after u in the ordering
+
+KAHN'S ALGORITHM:
+The idea of  Kahn’s algorithm is to repeatedly remove nodes that have zero in-degree. 
+'''
+
+#Kahn’s algorithm
+#https://algocoding.wordpress.com/2015/04/05/topological-sorting-python/
+def TopologicalSort(graph):
+    TopologicalSortedList = []  #result
+    ZeroInDegreeVertexList = [] #node with 0 in-degree/inbound neighbours
+    inDegree = { u : 0 for u in graph } #inDegree/inbound neighbours
+
+    #Step 1: Iterate graph and build in-degree for each node
+    for u in graph:
+        for v in graph[u]:
+            inDegree[v] += 1
+
+    #Step 2: Find node(s) with 0 in-degree
+    for k in inDegree:
+        #print(k,inDegree[k])
+        if (inDegree[k] == 0):
+            ZeroInDegreeVertexList.append(k)           
+
+    #Step 3: Process nodes with in-degree = 0
+    while ZeroInDegreeVertexList:
+        v = ZeroInDegreeVertexList.pop(0) #order in important!
+        TopologicalSortedList.append(v)
+        #Step 4: Update in-degree
+        for neighbour in graph[v]:
+            inDegree[neighbour] -= 1
+            if (inDegree[neighbour] == 0):
+                ZeroInDegreeVertexList.append(neighbour)
+
+    return TopologicalSortedList
+    
+
+#Adjacency list
+graph = {
+        'A': set([]),
+        'B': set(['A']),
+        'C': set(['B'])
+        }
+
+result = TopologicalSort(graph)
+print("Topological sort >>> ", result)
+# check if #nodes in result == #nodes in graph
+if (len(result) == len(graph)):
+    print("Directed Acyclic Graph!")
+else:
+    print("Graph has cycles!")
+
