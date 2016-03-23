18c92bc64dd1441d0c3b55977fe5e928cea569da
Harish Chakravarthy <harishvc@gmail.com>
2016-03-22 21:59:35 -0700

    add solution

--- find-median-unsorted-list.py
+++ find-median-unsorted-list.py
@@ -4,5 +4,7 @@ Question: Find median of unsorted list
 Reference:
 1. http://stackoverflow.com/questions/10662013/finding-the-median-of-an-unsorted-array
 2. http://cs.stackexchange.com/questions/1914/to-find-the-median-of-an-unsorted-array
+
+solution: modify quick sort.
 '''
 
