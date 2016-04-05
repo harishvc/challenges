0a89f640ae1242d53a3cab8c131e8eda0cbd2f2d
Harish Chakravarthy <harishvc@gmail.com>
2016-04-04 22:08:17 -0700

    clean up;optimize :thumbsup:

--- longest-subset-of-string-with-two-characters.py
+++ longest-subset-of-string-with-two-characters.py
@@ -1,55 +1,56 @@
-'''
-Question: Find the longest subset of a given string with two unique characters.
-'''
+#Longest substring with at the most 2 unique characters
 
 '''
-Notes: :notes:
-1. Store unique characters
-2. Change = keep track of the index position where characters change
-3. Start  = start of the new substring
-
-input = aabaaabbccccd
-substrings with 2 unique characters are
-1. aabaaabb
-2. bbcccc
+NOTES
+1. Since we are looking for 2 unique characters, there are 4 MAIN conditions to handle
+2. startIndex = starting index of new substring
+3. changeIndex = index position where the next character is different than the current character
+4. When you see the third uniqiue character change values of startIndex and changeIndex
+   - startIndex = changeIndex + 1
+   - changeIndex = currentIndex - 1
 '''
 
-def LongestSubstring(input):
-    seen = []
-    start = 0
-    change = 0
-    max_length = 0
-    max_substring = ""
-    last_seen = input[0]
-    for i in range(len(input)):
-        #Seen 
-        if (input[i] in seen):
-            if(i> 0 and input[i] != input[i-1]):
-                change = i
-        #New character (1st or 2nd)
-        elif(input[i] not in seen and len(seen) < 2):
-            seen.append(input[i])
-            if(i> 0 and input[i] != input[i-1]):
-                change = i #index position of last change
-        else:
-            tmp = input[start:i]  #substring
-            tmp2 = len(tmp) 
-            #max length of substring
-            if(tmp2 > max_length):
-                max_length = tmp2
-                max_substring = tmp
-            #Update seen characters
-            del seen[:]
-            seen.append(change)
-            seen.append(input[i])
-            #update start pointer
-            start = change
-            #update change pointer
-            change = i
-    return(max_substring,max_length)
+def LS2(a):
+	print("input >>>", a)
+	startIndex = 0  #start of new substring
+	changeIndex = 0 #index position where the next character is different than the current character
+	longestSubstring = None
+	longestSubstringlength = 0
+	seenCount  = 0 #count of characters seen
+	seen = set() #characters seen so far
+	for currentIndex in range(0,len(a)):
+		#condition 1: first unique character
+		if currentIndex == 0:
+			seen.add(a[currentIndex])
+			seenCount = 1
+			startIndex = 0
+		#condition 2: second unique character
+		elif a[currentIndex] != a[currentIndex-1] and seenCount == 1:
+			seen.add(a[currentIndex])
+			seenCount = 2
+			changeIndex = currentIndex-1
+		#condition 3: first followed by second or second followed by first
+		elif a[currentIndex] != a[currentIndex-1] and a[currentIndex] in seen and a[currentIndex-1] in seen:
+			changeIndex = currentIndex-1
+		#condition 4: third unique character
+		elif a[currentIndex] != a[currentIndex-1]:
+			assert a[currentIndex] not in seen and seenCount == 2, print("Error")
+			if currentIndex - startIndex > longestSubstringlength:
+				longestSubstringlength = currentIndex - startIndex
+				longestSubstring = a[startIndex:currentIndex]
+			seen.remove(a[changeIndex])
+			seen.add(a[currentIndex])
+			#optimization! - not starting over again
+			startIndex = changeIndex + 1
+			changeIndex = currentIndex - 1
+	#condition 5: input has only two characters repeating!
+	if (seenCount == 2 and longestSubstringlength == 0):
+		return(a)
+	#condition 6: input has only 1 character repeating!					
+	elif(seenCount == 1):
+		return
+	else:
+		return(longestSubstring)
 
-
-input = "aabaaabbccccd"
-print("input >>>", input)
-a,b = LongestSubstring(input)
-print ("Longest 2 characters subset>>", a)        
+a = "aabaaabbccccd"
+print(a, " >>> ", LS2(a))
