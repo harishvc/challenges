ebb4e4bcc7de6de46489121501ca8874d9908e58
Harish Chakravarthy <harishvc@gmail.com>
2016-05-05 22:27:55 -0700

    Optimize code :thumbsup: :notes:

--- Design-and-implement-LRU.py
+++ Design-and-implement-LRU.py
@@ -1,88 +1,116 @@
-#Question:Design and implement Least Used Cache (LRU)
-#Excellent explanation
-#http://mcicpc.cs.atu.edu/archives/2012/mcpc2012/lru/lru.html
+'''
+Design and implement Least Used Cache (LRU)
+REFERENCE:
+http://mcicpc.cs.atu.edu/archives/2012/mcpc2012/lru/lru.html
+'''
 
-import collections
-#Source:http://www.kunxi.org/blog/2014/05/lru-cache-in-python/
-#Solution 1: 
-# Advantages: Constant time for storage and time , simplicity
-# Disadvantages: Emphasis on timestamp. So there may be a situation where keys access frequently at the start but not actively latter will be removed
-class LRUCache1:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.cache = collections.OrderedDict()
+'''
+Implement Least Recentry Used Cache (LRU)
+CONSTRAINTS:
+1. get and set values on constant time
+2. evict (open up space) in < O(n)
+DESIGN:
+1. Hash of Double Linked  List (DLL)
+2. Hash value has reference to the DLL node
+3. DLL has data, prev, next and reference back to hash
+ALGORITHM:
+1. set(key,value):
+   - Create DLL node with key, value
+   - Move DLL node to head (most recently used)
+   - Store reference to DLL node in the hash
+2. get(key)
+   - Get DLL node from hash
+   - Move DLL node to head (most recently used)
+   - return value from DLL node
+3. evict() - remove the least used node
+   - remove DLL node
+   - update size
+   - remove hash key   
+'''
+class DLL:
+	def __init__(self,key,data):
+		self.data = data
+		self.next = None
+		self.prev = None
+		self.key = key  #reference to hash key
 
-#     def get(self, key):
-#         try:
-#             value = self.cache.pop(key)
-#             self.cache[key] = value
-#             return value
-#         except KeyError:
-#             return -1
+class LRU:
+	def __init__(self,capacity):
+		self.keys = {}  #hash of keys and values
+		self.capacity = capacity
+		self.size = 0
+		self.head = None #pointer to the first DLL (MRU)
 
-    def set(self, key, value):
-        try:
-            self.cache.pop(key)  #Always pop
-        except KeyError:
-            if len(self.cache) >= self.capacity:
-                self.cache.popitem(last=False)  #Remove element added first, similar to queue
-        self.cache[key] = value  #Always insert, ordered dictionary keeps track of the order
-                
-    def print(self,state):
-        print("%s ---> " % (state) ,end="")
-        for key in (self.cache):
-            print (key,end=" ")
-        print ("")        
+	def set(self,key,value):
+		#step 1: create DLL
+		node = DLL(key,value)
+		#step 2: match hash key with DLL (value)
+		self.keys[key] = node
+		#step 3: move DLL to the head
+		if (self.size == 0): #first node
+			self.head = node
+		else:
+			self.insertHead(node)
+		self.size += 1
 
+	def get(self,key):
+		#step 1: move node containing the key to the head
+		node = self.keys[key]
+		#case 1: node has prev and next
+		if node.prev and node.next:
+			node.prev.next = node.next
+			node.next.prev = node.prev
+			node.prev=None
+			node.next=None
+			self.insertHead(node)
+		#case 2: node is at the end		
+		elif node.next is None:
+			t = node.prev
+			t.next = None
+			node.prev = None
+			self.insertHead(node)
+		#case 3: node at top  - nothing to do!
+		return node.data
+	
+	def insertHead(self,node):
+		t = self.head
+		self.head = node
+		node.next = t
+		t.prev = node
 
-#Solution: 2
-#Disadvantages: Two dictionaries, time to find the entry with the least timestamp
-class LRUCache2:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.tm = 0        #timestamp
-        self.cache = {}    #dictionary to store key, value  
-        self.lru = {}      #dictionary to sore  key, timestamp
+	def evict(self):
+		node = self.head
+		#IMPORTANT: Find node before last node
+		while node.next.next:
+			node = node.next
+		tmp= node.next
+		#delete DLL and reference
+		del self.keys[tmp.key] #remove hash key
+		node.next = None #update node before last node
+		self.size -= 1 #reduce size
 
-    def get(self, key):
-        if key in self.cache:
-            self.lru[key] = self.tm
-            self.tm += 1
-            return self.cache[key]
-        return -1
+	def size(self):
+		return self.size
 
-    def set(self, key, value):
-        if len(self.cache) >= self.capacity:
-            # find the LRU entry
-            old_key = min(self.lru.keys(), key=lambda k:self.lru[k])  #find key with minimum timestamp in dictionary!
-            self.cache.pop(old_key)
-            self.lru.pop(old_key)
-        self.cache[key] = value
-        self.lru[key] = self.tm   #timestamp
-        self.tm += 1  #increment timestamp
+	def print(self):
+		node = self.head
+		while node:
+			print(node.data,end=" ")
+			node = node.next
+		print("")
 
-    def print(self,state):
-        print("%s ---> " % (state) ,end="")
-        for key in (self.cache):
-            print (key,end=" ")
-        print ("")        
-
-input = 'ABC!D!BE!DEF!'
-        
-start1 = LRUCache1(3)
-print ("Solution 1")
-for i,x in enumerate(list(input)):
-    if (x == "!"):
-        start1.print(input[0:i])
-    else:
-        start1.set(x,1)      
-
-start2 = LRUCache2(3)
-print ("Solution 2")
-for i,x in enumerate(list(input)):
-    if (x == "!"):
-        start2.print(input[0:i])
-    else:
-        start2.set(x,1)      
-
-
+myLRU = LRU(5)
+myLRU.set(1,1)
+myLRU.set(2,2)
+myLRU.set(3,3)
+print(">LRU Size:", myLRU.size)
+print(">LRU Values >>",end="")
+myLRU.print()
+print(">get ", myLRU.get(2))
+print(">LRU Values >>",end="")
+myLRU.print()
+print(">Evict")
+myLRU.evict()
+print(">LRU size:", myLRU.size)
+print(">LRU Values >>",end="")
+myLRU.print()
