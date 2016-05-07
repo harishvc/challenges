4429f290bd871a00c088983bea125706384ba914
Harish Chakravarthy <harishvc@gmail.com>
2016-05-07 00:22:54 -0700

    modify code;optimize & add notes

--- queue-implementation-using-linked-lists.py
+++ queue-implementation-using-linked-lists.py
@@ -1,85 +1,69 @@
-#Implement queue using linked lists
-#Source: https://github.com/careermonk/DataStructureAndAlgorithmicThinkingWithPython/blob/master/src/chapter05queues/QueuesWithLinkedLists.py
+'''
+Question:
+Implement a queue using a linked list
+'''
 
-# Node of a Singly Linked List
-class Node:
-	# constructor
-	def __init__(self, data=None, next=None):
+class LinkedList:
+	def __init__(self,data):
 		self.data = data
-		self.last = None
-		self.next = next
-	# method for setting the data field of the node    
-	def setData(self, data):
-		self.data = data
-	# method for getting the data field of the node   
-	def getData(self):
-		return self.data
-	# method for setting the next field of the node
-	def setNext(self, next):
-		self.next = next
-	# method for getting the next field of the node    
-	def getNext(self):
-		return self.next
-	# method for setting the last field of the node
-	def setLast(self, last):
-		self.last = last
-	# method for getting the last field of the node    
-	def getLast(self):
-		return self.last	
-	# returns true if the node points to another node
-	def hasNext(self):
-		return self.next != None
+		self.next = None
 
+class Queue:
+	def __init__(self):
+		self.head = None
 
-class Queue(object):
-	def __init__(self, data=None):
-		self.front = None
-		self.rear = None
-		self.size = 0
-
-	def enQueue(self, data):
-		self.lastNode = self.front
-		self.front = Node(data, self.front)
-		if self.lastNode:
-			self.lastNode.setLast(self.front)
-		if self.rear is None:
-			self.rear = self.front
-		self.size += 1
-
-	def queueRear(self):
-		if self.rear is None:
-			print ("Sorry, the queue is empty!")
-			raise IndexError
-		return self.rear.getData()
-
-	def queueFront(self):
-		if self.front is None:
-			print ("Sorry, the queue is empty!")
-			raise IndexError
-		return self.front.getData()
-
-	def deQueue(self):
-		if self.rear is None:
-			print ("Sorry, the queue is empty!")
-			raise IndexError
-		result = self.rear.getData()
-		self.rear = self.rear.last
-		self.size -= 1
-		return result
-
-	def size(self):
-		return self.size
+	#Add new node to the tail of the linked list (queue)
+	def enqueue(self,data):
+		node = LinkedList(data)
+		if self.head:
+			current = self.head
+			while(current.next is not None):
+				current = current.next
+			current.next = node
+		else:
+			self.head = node
 	
-que = Queue()
-que.enQueue("first")
-print ("Front: " + que.queueFront())
-print ("Rear: " + que.queueRear())
-que.enQueue("second")
-print ("Front: " + que.queueFront())
-print ("Rear: " + que.queueRear())
-que.enQueue("third")
-print ("Front: " + que.queueFront())
-print ("Rear: " + que.queueRear())
-print ("Dequeuing: " + que.deQueue())
-print ("Front: " + que.queueFront())
-print ("Rear: " + que.queueRear())
+	def dequeue(self):
+		#case 1: one more node after
+		if self.head and self.head.next:
+			node = self.head
+			self.head = self.head.next
+			node.next = None
+			#print("###", self.head.data)
+			return node.data
+		#case 2: last node	
+		elif self.head:
+			tmp = self.head
+			self.head = None
+			return tmp.data
+		#case 3: queue empty!
+		else:
+			print("Error: queue empty")
+			return None, None
+
+	def PrintLinkedList(self):
+		list = self.head
+		while list is not None:
+			if list.next is not None:
+				print(list.data,end=" -> ")
+			else:
+				print(list.data,end="\n")
+			list = list.next
+
+
+#create queue using linked list
+q = Queue()
+for i in range(1,4):
+	q.enqueue(i)
+print("input >>> " , end="")
+q.PrintLinkedList()
+print("$>dequeue 5 >>> " , end="")
+q.enqueue(5)
+q.PrintLinkedList()
+print("$>enqueue ")
+print(q.dequeue())
+print("$>enqueue ")
+print(q.dequeue())
+print("input >>> " , end="")
+q.PrintLinkedList()
+
