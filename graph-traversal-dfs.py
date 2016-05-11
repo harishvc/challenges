07418910491f98a5aaad745f5663e2610b031a74
Harish Chakravarthy <harishvc@gmail.com>
2016-05-11 06:39:09 -0700

    clean up;easy to scan and understand

--- graph-traversal-dfs.py
+++ graph-traversal-dfs.py
@@ -1,6 +1,17 @@
 '''
-Using DFS find all path between two edges.
+Use DFS find all path between two edges.
+
+REFERENCE:
+1. http://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
+2. https://www.cs.berkeley.edu/~vazirani/algorithms/chap4.pdf
+
+NOTES:
+1. Depth-first search makes deep incursions into a graph, retreating 
+   only when it runs out of new nodes to visit. 
+2. DFS can end up taking a long and convoluted route to a vertex 
+   that is actually very close by.
 '''
+
 #Latency list
 graph = {'A': set(['B', 'C']),
          'B': set(['A', 'D', 'E']),
@@ -9,22 +20,24 @@ graph = {'A': set(['B', 'C']),
          'E': set(['B', 'F']),
          'F': set(['C', 'E'])}
 
-#http://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
-def dfsPath(graph,start,finish,path=None):
-	if path is None:
-		path = [start]
-	if (start == finish):
-		yield path
-	#values are unordered in a set
-	#store in list and compare convert to set	
-	for next in graph[start] - set(path):
-		yield from dfsPath(graph,next,finish,path+[next])
 
-#All paths
+def graphPathDFS(graph,start,end,visited,path):
+	if start == end:
+		yield path
+	elif start not in visited:
+		visited.add(start)
+		for node in graph[start]:
+			yield from graphPathDFS(graph,node,end,visited,path+[node])
+
 start = "A"
-finish = "F"
-print("All paths between %s & %s = %s" % (start,finish,list(dfsPath(graph,start,finish))))
+end="F"
+paths = graphPathDFS(graph,start,end,set(),[start])
+
+print("All path between %s & %s (DFS)" % (start,end))
+for path in paths:
+	print("->".join(path))
 #Shortest path
-t =  list(dfsPath(graph,start,finish))
+t =  list(graphPathDFS(graph,start,end,set(),[start]))
 t.sort(key=len)
-print("Shortest path=",t[0])
+print("Shortest path=","->".join(t[0]))
+
