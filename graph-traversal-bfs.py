a8a4ef3f4d7a1a9225ac1a2df63d394c3bb76de4
Harish Chakravarthy <harishvc@gmail.com>
2016-05-11 06:39:31 -0700

    clean up;easy to scan and understand

--- graph-traversal-bfs.py
+++ graph-traversal-bfs.py
@@ -1,5 +1,18 @@
 '''
-Using BFS find all path between two edges & the shortest path 
+Use BFS find all path between two edges.
+
+REFERENCE:
+1. http://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
+2. https://www.cs.berkeley.edu/~vazirani/algorithms/chap4.pdf
+
+NOTES:
+1. Breadth-first search visitS vertices in increasing order 
+   of their distance from the starting point. 
+2. Breadth-first search always provides the shortest path between
+   two connected vertices   
+3. Breadth-first search is a broader, shallower search, rather like the 
+   propagation of a wave upon water.
+4. Queue is used for implementation
 '''
 
 #Latency list
@@ -10,26 +23,29 @@ graph = {'A': set(['B', 'C']),
          'E': set(['B', 'F']),
          'F': set(['C', 'E'])}
 
-
-
-#http://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
-def bfsPath(graph, start, goal):
-    queue = [(start, [start])]
-    while queue:
-        (vertex, path) = queue.pop(0) #IMPORTANT!!!
-        for next in graph[vertex] - set(path):
-            if next == goal:
-                yield path + [next]
-            else:
-                queue.append((next, path + [next]))
-
-def shortest_path(graph, start, goal):
-    try:
-        return next(bfsPath(graph, start, goal))
-    except StopIteration:
-        return None
+import queue
+def graphPathBFS(graph,start,end):
+	q = queue.Queue()
+	visited = set()
+	#current node and path added to queue
+	q.put([start,start])
+	while not q.empty():
+		next,path = q.get()
+		if next == end:
+				yield path
+		else:
+			visited.add(next)
+			for node in graph[next] - visited:
+				#place node and path in the queue
+				q.put([node,path+node])
 
 start = "A"
-finish = "F"
-print("All paths between %s & %s = %s" % (start,finish,list(bfsPath(graph,start,finish))))
-print("Shortest path=",shortest_path(graph, start, finish)) 
+end="F"
+paths = graphPathBFS(graph,start,end)
+print("All path between %s & %s (BFS)" % (start,end))
+for path in paths:
+	print("->".join(path))
+#Shortest path
+t =  list(graphPathBFS(graph,start,end))
+t.sort(key=len)
+print("Shortest path=","->".join(t[0]))
