35dbe32dc1ee27ef46e4c8b96f361d174126c82e
Harish Chakravarthy <harishvc@gmail.com>
2016-06-05 11:32:40 -0700

    Add notes;clean up code :thumbsup:

--- two-dimensional-array-number-exist.py
+++ two-dimensional-array-number-exist.py
@@ -12,19 +12,30 @@ def init():
     count += 1
     return count
 
-#Solution 1: start fro top right and start eliminating rows and cols
-def Search(Matrix,input):
-    x,y = 0,cols-1  #Top right corner
-    while (x >=0 and y >=0 and x < rows and y < cols):
-        if ( input > Matrix[x][y]):
-            #next row
-            x += 1
-        elif( input < Matrix[x][y]):
-            #next col
-            y -= 1
-        elif(input == Matrix[x][y]):
-            return True
-    return False
+
+#OBSERVARTION:
+#1. At any given index i and j. All values a[0][j] .... a[i-1][j] are less than a[i][j]
+#2. At any given index i and j. All values a[i][0] .... a[i][j+1] are greater than a[i][j]
+#3. If we start at top right, we can elimainate a row or column (Binary Search technique)
+
+#Solution 1: start from top right and start eliminating rows and cols
+#Time complexity: O(m+n) , m=#rows, n=#cols 
+def Search(Matrix,target):
+	#rows
+	rows = len(Matrix)
+	#cols
+	cols = len(Matrix[0])
+	x,y = 0,cols-1  #Top right corner
+	while (x < rows and y >=0):
+		if ( target > Matrix[x][y]):
+			#next row
+			x += 1
+		elif( target < Matrix[x][y]):
+			#next col
+			y -= 1
+		elif(target == Matrix[x][y]):
+			return True
+	return False
 
 #solution 2: Modified BST
 #convert index to matrix coordinates
@@ -32,7 +43,7 @@ def index2Value(matrix,middle):
 	cols = len(matrix[0])
 	return matrix[middle//cols][(middle%cols)]
 
-#Modified BST
+
 def Search2(Matrix,target,start,end):
 	print("### start=%d end=%d" %(start,end))
 	if start == end and index2Value(matrix,start) == target:
@@ -61,7 +72,7 @@ print("Is %d there? %s" % (10,Search(Matrix,10)))
 print("Is %d there? %s" % (50,Search(Matrix,50)))
 
 #Solution 2
-rows = len(Matrix)
-cols = len(Matrix[0])
-target = 9
-Search2(Matrix,target,0,rows*cols-1)
+#rows = len(Matrix)
+#cols = len(Matrix[0])
+#target = 9
+#Search2(Matrix,target,0,rows*cols-1)
