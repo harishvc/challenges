316060d6d88ed5baff8d8a2d66cc9f9c83bc4c6f
Harish Chakravarthy <harishvc@gmail.com>
2016-06-17 19:09:33 -0700

    create library to doless :thumbsup:

---
+++ mylib/doless.py
@@ -0,0 +1,7 @@
+#print 2 dimentional matrix
+def PrintMatrix(Table):
+    for row in range(0,len(Table)):
+        for col in range(0,len(Table[row])):
+            print("%d" % (Table[row][col]),end=" ")
+        print("")
+
