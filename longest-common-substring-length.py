2fc65a93902901fe2a047d2fec98a1b4cd6e0aba
Harish Chakravarthy <harishvc@gmail.com>
2016-06-18 22:48:06 -0700

    Optimize solution to use less space and include observation :notes:

--- longest-common-substring-length.py
+++ longest-common-substring-length.py
@@ -1,4 +1,4 @@
-#Question: Given two sequences find the length of the longest common substring
+#Question: Given two sequences find the longest common substring
 
 '''
 References
@@ -7,36 +7,42 @@ References
 3. Tushar Roy - https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/LongestCommonSubstring.java
 4. Lec 15 | MIT 6.046J - https://www.youtube.com/watch?v=V5hZoJ6uK-s
 5. WikiBooks - https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring#Python3
+
+OBSERVATION:
+1. Characters have to be continuous for substring
+2. if continuous characters match we increment 
+   length of max substring else ignore (since we are only calculating max length)
+3. Max substring can end at any location (not necessarily at the end) so need to track it! 
 '''
 
-#Print two dimensional matrix        
-def PrintMatrix(Table):
-    for row in range(0,len(Table)):
-        for col in range(0,len(Table[row])):
-            print("%d" % (Table[row][col]),end=" ")
-        print("")
-
-
-#Time Complexity: O(mn), Space Complexity: O(mn)
-#Bottom-up (Dynamic Programming)
-#Start with the lowest values of the input and keep building the solutions for higher values
-def LCS(X, Y):
-    m = len(X)
-    n = len(Y)
-    longest, x_longest = 0, 0
-    # An (m+1) times (n+1) matrix
-    C = [[0 for j in range(n+1)] for i in range(m+1)]
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if X[i-1] == Y[j-1]: 
-                C[i][j] = C[i-1][j-1] + 1
+#Space complexity: O(n) 
+#Time complexity: O(mn)
+def LongestSubstring(m,n):
+    msize = len(m)
+    nsize = len(n)
+    row1 = [0] * nsize #col size
+    row2 = [0] * nsize #col size
+    maxsize = 0
+    for i in range(0,msize):
+        for j in range(0,nsize):
+            if n[j] == m[i]:
+                row2.append(row1[j-1] + 1)
+                #maxsize = row2[j] if row2[j] > maxsize else maxsize
+                if row2[j] > maxsize: maxsize = row2[j] 
             else:
-                C[i][j] = 0
-    #PrintMatrix(C)
-    return C[m][n]
+                #doesn't matter for substring since characters have to be continuous
+                row2.append(0)
+        #IMPORTANT: switch rows
+        row1,row2 = row2,row1
+        #IMPORTANT: empty the second list; size=0
+        del row2[:]  #row2.clear()
+    #IMPORTANT: since rows get switched use row1
+    return maxsize
 
     
 X = "ABCDAF"
 Y = "ZBCDAF"
 print("Input strings => %s  %s" % (X,Y))
-print("Length of the longest common substring => %d" % (LCS(X,Y)))
+print("Length of longest common substring => %s" % (LongestSubstring(Y,X)))
+
+
