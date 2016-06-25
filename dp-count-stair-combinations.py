20081fa418b996596745fc433caf043baf97a467
Harish Chakravarthy <harishvc@gmail.com>
2016-06-25 00:33:45 -0700

    Add :notes: and recursive solution

--- dp-count-stair-combinations.py
+++ dp-count-stair-combinations.py
@@ -3,12 +3,26 @@
 #http://www.geeksforgeeks.org/count-ways-reach-nth-stair/
 
 #Count stair combinations
+
+#Solution 1: Recursive 
+#Values are re-calcuated, not efficient
+def countStairCombinationsRecursive(n):
+	if n ==0:
+		#top of the stairs
+		return 1 #ONE PATH
+	elif n < 0:
+		return 0
+	else:
+		#take 1 step or two steps
+		return countStairCombinationsRecursive(n-1) + countStairCombinationsRecursive(n-2)
+
 '''
 OBSERVATION:
 1. At each stair you can take 1 step or 2 step
 2. You reach top of stairs when #steps = 0
 3. If you start top down, you can add already computer #combinations for n-1 and n-2
 '''
+#Solution 2: Dynamic Programming
 def countStairCombinations(n):
 	table = [0 for i in range(n+1)]
 	table[0] = 0
