6c043c818a501911eed6cf153c653529f804c865
Harish Chakravarthy <harishvc@gmail.com>
2016-06-29 06:51:32 -0700

    clean up code and optimize :thumbsup:

--- dp-all-balanced-partition.py
+++ dp-all-balanced-partition.py
@@ -1,41 +1,43 @@
 #Given a list, identify two sub-lists with equal sum (balanced partition) 
 
-
-
 '''
 OBSERVATION:
 1. Find all the subsets recursively
 2. On the way back (from the last node) check the sum
 3. Similar to POST ORDER traversal!
 
+During each recursive call
+  - Increment depth
+  - Increment start
+  - Decrement target
+  - Add new node to visited node (at current depth)
+
 REFERENCE:
 1. http://www.geeksforgeeks.org/backttracking-set-4-subset-sum/
 '''
 
 '''
-a = input list
-result = store nodes along the recursion path
-asize = size of input list
-rsize = size of the result list
-tsum = sum so far
-nodes = #nodes seen so far
-target = target
+target  = sum for balanced partition
+start   = start index
+end     = end index
+depth   = depth of the recursion tree
+results = all the nodes visited so far
 '''
-def generateSubsets(a,result,asize,rsize,tsum,nodes,target):
-	if tsum == target:
-		#print (result[0:rsize])
-		yield (result[0:rsize])
-		#return
-	else:
-		for i in range(nodes,asize):
-			#IMPORTANT: insert rather than append
-			result.insert(rsize,a[i])
-			yield from generateSubsets(a,result,asize,rsize+1,tsum+a[i],i+1,target)  
+def findAllBalancedPartitions(a,target,start,end,depth,result):
+	#Edge case: sum = 0
+	if target == 0 and start > 0:
+		yield result[0:depth]
+		return
+	for i in range(start,end):
+		#IMPORTANT: list insert modifies the list and returns None
+		result.insert(depth,a[i])
+		yield from findAllBalancedPartitions(a,target-a[i],i+1,end,depth+1,result)
+	
 
-myinput = [4,1,-5,6,-11,3]
-print("input >>> %s, Listing all sub-sets with sum=%d" % (myinput,sum(myinput)//2))
-z = generateSubsets(myinput,[],len(myinput),0,0,0,sum(myinput)//2)
-for i in z:
-	print(i)
-
-
+a = [4,1,-5,6,-11,3]
+print("Input >>>", a)
+print("Listing all balanced partitions ...")
+for x in findAllBalancedPartitions(a,sum(a)//2,0,len(a),0,[]):
+	#find rest of the values
+	y = [i for  i in a if i not in x]
+	print(x,y)
