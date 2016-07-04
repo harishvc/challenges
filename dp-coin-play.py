50f638d27d1930207ba5f10a60c4b0fbfea480e3
Harish Chakravarthy <harishvc@gmail.com>
2016-07-04 08:07:21 -0700

    clean up;add comments;

--- dp-coin-play.py
+++ dp-coin-play.py
@@ -23,7 +23,7 @@ REFERENCE:
 1. http://www.geeksforgeeks.org/dynamic-programming-set-31-optimal-strategy-for-a-game/
 2. https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/NPotGold.java
 3. http://stackoverflow.com/questions/22195300/understanding-solution-to-finding-optimal-strategy-for-game-involving-picking-po
-
+4. http://ideone.com/ug1DOx
 '''
 
 
@@ -41,27 +41,28 @@ def MaxValue(values,start,end):
 	return (max(path1,path2))
 
 
-#DP Table
-#https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/NPotGold.java
+#Solution 2: Dynamic Programming
 def MaxValueDP(values,size):
-	table = [[[0,0] for x in range(size)] for y in range(size)]
-	#Initialize - for size 1, max for player1 and max player2
-	for i in range(size):
-		table[i][i] = [values[i],0]
-	#Generate values for size 2 , 3 ....	
+	table = [[0 for x in range(size)] for y in range(size)]
+	#Initialize for length=1
+	for start in range(size):
+		table[start][start] = values[start]
+	#Generate values for length=2,3 ....	
 	for l in range(2,size+1):
-		for i in range(size-2):
-			j = i + l - 1
-			#print(i,j)
+		#start value range 0  & 1
+		for start in range(size-2):
+			#end values changes based on start and length(l)
+			#size =2  -> [0,1] [1,2],[2,3]
+			#size =3  -> [0,2], [1,3]
+			end = start + l - 1
 			#IMPORTANT: break!
-			if j == size:
+			if end == size:
 				break
-			a,b = table[i+1][j]
-			x,y = table[i][j-1]
-			p1 = max(values[i]+b , values[j]+y)
-			p2 = min(a,x)
-			table[i][j] = [p1,p2]
-			#print("\t",p1,p2)
+			#option 1: pick start	
+			path1 = values[start] + min(table[start+2][end],table[start+1][end-1])
+			#option 2: pick end
+			path2 = values[end] + min(table[start+1][end-1],table[start][end-2])
+			table[start][end] = max(path1,path2)
 	#print(table)
 	return table[0][size-1]
 
@@ -73,7 +74,7 @@ a = [3,9,1,2]
 #print(MaxValue(a,0,len(a)-1))
 
 #Solution 2: DP
-player1,player2 = MaxValueDP(a,len(a))
+player1 = MaxValueDP(a,len(a))
 print("values >>>", a)
-print("player1=%d player2=%d" % (player1,player2))
+print("player1=%d" % (player1))
 
