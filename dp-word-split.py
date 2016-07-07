d0c1fe790d285f6cd820e0dbb017a16f5f919228
Harish Chakravarthy <harishvc@gmail.com>
2016-07-07 05:51:53 -0700

    optimize code; add :notes: :rocket:

--- dp-word-split.py
+++ dp-word-split.py
@@ -1,45 +1,47 @@
 #Can the input word be split using space-separated sequence of dictionary words? 
 
-#Solution 1: simple. Will **not** work if dictionary contains more than suffix of a word
-def canSplit1(a,d,start,end):
-	current = start
-	while current < end:
-		if a[start:end] in d:
-			return True
-		elif a[start:current+1] in d and a[current+1:end] in d:
-			return True
-		elif a[start:current+1] in d:
-			current +=1
-			start = current
-		else:
-			current +=1 
-	return False
+'''
+NOTES
+Solve using Dynamic Programming
+1. Use two loops - forward and backward
+     - forward: check new word is dictionary
+     - backward: check if new word can be split into **TWO** words based on 
+       what we have seen and recorded so far 
 
-#Solution 2: Dynamic Programming
+REFERENCE
+1. https://www.youtube.com/watch?v=WepWFGxiwRs
+2. https://codesays.com/2014/solution-to-word-break-by-leetcode/
+
+Time complexity: O(n^2)
+Space complexity: O(n)
 '''
-1. Keep track of the longest match seen so far!
-2. ALOGITHM
-   - Check all permutations with dictionary. If found update index positions as 1 in a lookup table
-   - If you have -1 in the lookup table, you can't split the word!
-'''
-def canSplit2(word,dictionary):
-	size = len(word)
-	table = [-1]*size #lookup table. Inititalize -1
-	#step 1: Find all 
+
+def wordSplit(a,d):
+	size = len(a)
+	table = [False] * size
 	for i in range(0,size):
-		for j in range(i,size):
-			if word[i:j+1] in dictionary:
-				#print(word[i:j+1])
-				for t in range(i,j+1):
-					table[t] = 1
-	#print(table)	
-	#step 2
-	if -1 in table:
-		return False
-	else:
-		return True
+		#case 1: Best case - word found!
+		if a[:i+1] in d:
+			table[i] = True
+			continue
+		#word not found: Can the new word be split into 
+		#TWO words based on "what we have seen and recorded so far"?
+		for j in range(i):
+			#case 2: prefix not found!
+			if table[j] == False:
+				continue
+			#case 3: prefix found, is suffix in dictionary?
+			elif a[j+1:i+1] not in d:
+				continue
+			#case 4: word can be split into prefix and suffix found in dictionary
+			else:
+				table[i] = True
+				break
+	#print(table)
+	#return last index value
+	return table[-1]
+
 
 a = "iloveapple"
-d = ["i", "lov", "love", "apple"]
-#print("input > %s canSplit? %s" % (a,canSplit1(a,d,0,len(a))))
-print("input >>> %s canSplit? %s" % (a,canSplit2(a,d)))
+d = ["i", "lov", "love", "loveapple"]
+print("input >>> %s canSplit? %s" % (a,wordSplit(a,d)))
