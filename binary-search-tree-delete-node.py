5fefd883a5b3745e904a5a9b5a754e624220b347
Harish Chakravarthy <harishvc@gmail.com>
2016-07-09 20:35:48 -0700

    update BST Library

--- binary-search-tree-delete-node.py
+++ binary-search-tree-delete-node.py
@@ -1,105 +1,29 @@
-'''
-Question: Delete a node from Binary Search Tree(BST)
-
-Time Complexity: O(logn), since only branch of a tree is processed
-'''
-
-class BinarySearchTree:
-    def __init__(root, data):
-        root.left = None
-        root.right = None
-        root.data = data
-
-def insertNode(root, node):
-    if root is None:
-        root = node
-    else:
-        if root.data > node.data:
-            if root.left == None:
-                root.left = node
-            else:
-                insertNode(root.left, node)
-        else:
-            if root.right == None:
-                root.right = node
-            else:
-                insertNode(root.right, node)
-#Source: http://stackoverflow.com/questions/11392773/deletion-in-binary-search-tree-with-parent-pointers
-#Reference: https://www.youtube.com/watch?v=gcULXE7ViZw
-def delete(x,T):
-    #Condition 1: Value does not exist in the tree
-    if T is None:
-        print('Element Not Found')
-    #Condition 2: Value is less, go right    
-    elif x<T.data:
-        T.left = delete(x,T.left)
-    #Condition 3: Value is more, go left    
-    elif x>T.data:
-        delete(x,T.right)
-    #Condition 4: Found node to delete and node is full node    
-    elif T.left and T.right:
-        TempNode = findMinNode(T.right)
-        T.data = TempNode.data
-        T.right = delete(TempNode.data,T.right)
-    #Condition 5: Found node to delete and node is half node or leaf
-    else:
-        if T.left is None:
-            T = T.right
-        elif T.right is None:
-            T = T.left
-    return T
-
-def findMinNode(root):
-    if root.left:
-        return findMinNode(root.left)
-    else:
-        return root
+#Question: Delete a node from Binary Search Tree(BST)
 
 
-#Ascending order of elements
-def inOrderTraversal(root):
-    if not root:
-        return
-    inOrderTraversal(root.left)
-    print(root.data,end=" ")
-    inOrderTraversal(root.right)
-    
-    
-#input = [15,10,20,21,8,12,7,19,11,17,14,16,18]
-input = [12,5,17,3,7,13,19,1,9,14,20,8,11,18]
+import sys
+sys.path.append("./mylib")
+import BinaryTreeTraversal 
+import BST
 
-root = BinarySearchTree(input[0])
-
+a = [3,2,1,5,5,4,7]
 #Create BST
-for x in input[1:]:
-    insertNode(root,BinarySearchTree(x))
+root = BST.BSTNode(a[0])
+for i in range(1,len(a)):
+	node = BST.BSTNode(a[i])
+	BST.insert(root,node)
 
-#Print tree
-print("Inorder traversal of BST >>> ",end="")
-inOrderTraversal(root)
+
+
+print("Tree in-order traversal:")
+BinaryTreeTraversal.inOrder(root)
+
+#Time Complexity: O(logn), since only branch of a tree is processed
+#Delete node from BST
+print("\ndeleting node.data=", 3)
+root = BST.delete(root,BST.BSTNode(3))
+
+print("Tree in-order traversal:")
+BinaryTreeTraversal.inOrder(root)
 print("")
-print("####################")
 
-#Delete leaf node
-print("Deleting leaf node 1")
-delete(1,root)
-print("Inorder traversal of new BST >>> ",end="")
-inOrderTraversal(root)
-print("")
-print("####################")
-
-#Delete half node
-print("Deleting half node 13")
-delete(13,root)
-print("Inorder traversal of new BST >>> ",end="")
-inOrderTraversal(root)
-print("")
-print("####################")
-
-
-#Delete full node
-print("Deleting full node 17")
-delete(17,root)
-print("Inorder traversal of new BST >>> ",end="")
-inOrderTraversal(root)
-print("")
--- mylib/BST.py
+++ mylib/BST.py
@@ -1,4 +1,5 @@
 #Binary Search Tree Library
+#Add, Delete, isBST? max, min
 
 class BSTNode:
     def __init__(root, data):
@@ -6,18 +7,98 @@ class BSTNode:
         root.right = None
         root.data = data
 
-def insertNode(root, node):
-    if root is None:
-        root = node
-    else:
-        if root.data > node.data:
-            if root.left == None:
-                root.left = node
-            else:
-                insertNode(root.left, node)
-        else:
-            if root.right == None:
-                root.right = node
-            else:
-                insertNode(root.right, node)
+#Insert node to BST
+def insert(node,Newnode):
+	#condition 1: node is empty
+	if node is None:
+		node = Newnode
+	#condition 2: new value < value at parent node	and parent has NO left node
+	elif Newnode.data < node.data and node.left is None:
+		node.left = Newnode
+	#condition 3: new value < value at parent node	and parent has left node
+	elif Newnode.data < node.data:
+		insert(node.left,Newnode)
+	#condition 4: new value >= value at parent node	and parent has NO right node
+	#Handle DUPLICATE values. BST usually don't have DUPLICATE values by some definition
+	elif Newnode.data >= node.data and node.right is None:
+		node.right = Newnode
+	#condition 5: new value > value at parent node and parent has right node
+	else:
+		insert(node.right,Newnode)
 
+#Delete node from BST
+def delete(node,Newnode):
+	#case 1: Found node to delete
+	if node.data == Newnode.data:
+		#case 4: node to delete has left and right subtrees
+		if node.right and node.left:
+			#IMPORTANT: Find the smallest value in the right sub tree to replace the node for deletion
+			pnode,snode = findSmallest(node,node.right)
+			if pnode.left == snode:
+				#pnode and snode are ALREADY connected
+				#OBSERVATION: snode is a leaf or half node (cannot be a full node)
+				pnode.left = snode.right
+			else:
+				#OBSERVATION: NO left node; pnode == node
+				pnode.right = snode.right
+			snode.left = node.left
+			snode.right = node.right
+			#IMPORTANT: return snode
+			return snode
+		#case 5: node to delete has either left or right subtree or leaf	
+		elif node.left:
+			return node.left #promote left subtree
+		else:
+			return node.right  #promote right subtree; node == leaf
+	#case 2: node to delete in left subtree
+	elif Newnode.data < node.data and node.left:
+		node.left = delete(node.left,Newnode)
+	#case 3: node to delete in right subtree
+	elif Newnode.data > node.data and node.right:
+		node.right = delete(node.right,Newnode)
+	else:
+		print("Value outside BST")
+		return node
+	#IMPORTANT return node
+	return node
+
+#Find the node with smallest value in the subtree
+def findSmallest(predecessor, successor):
+	#Smallest value is in the left sub tree!
+	if successor.left:
+		return findSmallest(successor,successor.left)
+	else:
+		return predecessor,successor
+
+
+#Is the tree a BST?
+#Modify pre order traversal
+def isBST(node):
+	#condition 1: no node
+	if node is None:
+		return True
+	#condition 2: left node with value > parent node
+	elif node.left and node.left.data > node.data:
+		return False
+	#condition 3: right node with value < parent node
+	elif node.right and node.right.data < node.data:
+		return False
+	#condition 4: leaf node
+	else:
+		return isBST(node.left) and isBST(node.right)
+
+#Find Max
+#Modify in order traversal to get the value of the furthest right node
+def getMax(node):
+	if node.right:
+		return getMax(node.right)
+	else:
+		return node.data
+
+#Find Min
+#Modify post traversal to get the value of the furthest left node
+def getMin(node):
+	if node.left:
+		return getMin(node.left)
+	else:
+		return node.data
