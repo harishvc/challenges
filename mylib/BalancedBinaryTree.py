b6dac2401e3ea336337784b0241154dd617d1cfe
Harish Chakravarthy <harishvc@gmail.com>
2016-07-10 21:46:15 -0700

    Balanced Binary Tree Library

---
+++ mylib/BalancedBinaryTree.py
@@ -0,0 +1,48 @@
+#Balanced Binary Tree: Add, Delete, isBalanced?
+
+class BalancedBinaryTree:
+	def __init__(self,data):
+		self.data = data
+		self.left = None
+		self.right = None
+
+	def insertRight(self,node):
+		self.right = node
+
+	def insertLeft(self,node):
+		self.left = node
+
+
+def height(node):
+	if node is None:
+		return 0
+	lheight = height(node.left)  + 1
+	rheight = height(node.right) + 1
+	print("height(%d): lheight=%d rheight=%d" % (node.data,lheight,rheight))
+	return max(lheight,rheight)
+
+def isBalancedBinaryTree(node):
+	lheight = height(node.left)
+	rheight = height(node.right)
+	if abs(lheight - rheight) <= 1:
+		return True
+	else:
+		return False
+
+root = BalancedBinaryTree(1)
+t2 = BalancedBinaryTree(2)
+t3 = BalancedBinaryTree(3)
+t4 = BalancedBinaryTree(4)
+t5 = BalancedBinaryTree(5)
+t6 = BalancedBinaryTree(6)
+t7 = BalancedBinaryTree(7)
+#
+root.insertLeft(t2)
+root.insertRight(t3)
+t2.insertRight(t4)
+t2.insertLeft(t7)
+
+t4.insertLeft(t6)
+t3.insertLeft(t5)
+
+print(isBalancedBinaryTree(root))
