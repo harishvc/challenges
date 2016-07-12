bf84b1f2020677577ce92d609cd6b68e7f50832a
Harish Chakravarthy <harishvc@gmail.com>
2016-07-12 07:24:13 -0700

    clean up, easy to read and follow :thumbsup:

--- binary-search-tree-minimal-height.py
+++ binary-search-tree-minimal-height.py
@@ -1,72 +1,35 @@
+#Given a sorted list create a BST with minimal height
+
 '''
-Question: Given a sorted list create a BST with minimal height
-
-Time Complexity: O(nlogn), log n for each insert
+NOTES:
+1. TO build a balanced binary tree, root of the tree needs to the median of the sorted input
+2. Left and right nodes of the root can be easily constructed from the values on the left and right of the median
 '''
 
-class BinarySearchTree:
-    def __init__(root, data):
-        root.left = None
-        root.right = None
-        root.data = data
+import sys
+sys.path.append("./mylib")
+import BST
+import BinaryTreeTraversal
+import BinaryTreeEssentials
+import BalancedBinaryTree
 
-#BST insert
-def insertNode(root, node):
-    if root is None:
-        root = node
-    else:
-        if root.data > node.data:
-            if root.left == None:
-                root.left = node
-            else:
-                insertNode(root.left, node)
-        else:
-            if root.right == None:
-                root.right = node
-            else:
-                insertNode(root.right, node)
-    #return root
+def BalancedBST(a,start,end):
+	#IMPORTANT: exit condition > (not == or <)
+	if start > end:
+		return None
+	mid = start + (end-start)//2
+	node = BST.BSTNode(a[mid])
+	node.left = BalancedBST(a,start,mid-1)
+	node.right = BalancedBST(a,mid+1,end)
+	return node
 
-#Find BST height
-def height(root):
-    if (root == None) :
-        return 0
-    x = height(root.left)
-    y = height(root.right)
-    z = max(x,y) + 1
-    return z
 
-#Ascending order of elements
-def inOrderTraversal(root):
-    if not root:
-        return
-    inOrderTraversal(root.left)
-    print(root.data,end=" ")
-    inOrderTraversal(root.right)
+a = [4,5,6,7,8]
+root = BalancedBST(a,0,len(a)-1)
 
-#Algorithm  :idea:
-#1.Find median of list, insert median
-#  1.1 Go left of median
-#  1.2 Go right of median
-def BSTMinimalHeight(node,input,start,end):
-    if (end < start):
-        return None
-    median = (start + end) // 2   #floor
-    MedianNode = BinarySearchTree(input[median])
-    insertNode(node,MedianNode)
-    #go left of median
-    BSTMinimalHeight(MedianNode,input,start,median-1)
-    #go right of median
-    BSTMinimalHeight(MedianNode,input,median+1,end)
-    return MedianNode
-    
-    
-    
-input = [1,2,3,4,5]
-root = BSTMinimalHeight(None,input,0,len(input)-1)
-
-print("input >>>", input)
-print("Inorder traversal of BST >>> ",end="")
-inOrderTraversal(root)
+print("Input >>>", a)
+print("In order traversal of new BST>>")
+BinaryTreeTraversal.inOrder(root)
 print("")
-print("Height =", height(root))
+print("Height of new BST:", BinaryTreeEssentials.findHeight(root))
+print("Is balanced?", BalancedBinaryTree.isBalancedBinaryTree(root))
