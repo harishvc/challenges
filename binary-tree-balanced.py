c6cec7595be364ea946d391a2044a4fd931b21f8
Harish Chakravarthy <harishvc@gmail.com>
2016-07-12 07:25:18 -0700

    Major rewrite, add notes, easy to read and follow

--- binary-tree-balanced.py
+++ binary-tree-balanced.py
@@ -1,129 +1,35 @@
-'''
-Question: Check if a binary tree is a fully balanced binary tree
+#Check if a binary tree is a fully balanced binary tree
 
+'''
 Fully balanced tree is defined as a tree such that the height of the two subtrees of any node never differ by more than one.
-
-'''
-'''
-Question: Check if a binary tree is symmetric. 
 '''
 
-'''Binary Tree Class and its methods'''
-class BinaryTree:
-    def __init__(self, data):
-        self.data = data  # root node
-        self.left = None  # left child
-        self.right = None  # right child
-    # set data
-    def setData(self, data):
-        self.data = data
-    # get data   
-    def getData(self):
-        return self.data    
-    # get left child of a node
-    def getLeft(self):
-        return self.left
-    # get right child of a node
-    def getRight(self):
-        return self.right
-    # get left child of a node
-    def setLeft(self, left):
-        self.left = left
-    # get right child of a node
-    def setRight(self, right):
-        self.right = right
-    def insertLeft(self, newNode):
-        if self.left == None:
-            self.left = BinaryTree(newNode)
-        else:
-            temp = BinaryTree(newNode)
-            temp.left = self.left
-            self.left = temp
-    def insertRight(self, newNode):
-        if self.right == None:
-            self.right = BinaryTree(newNode)
-        else:
-            temp = BinaryTree(newNode)
-            temp.right = self.right
-            self.right = temp
+import sys
+sys.path.append("./mylib")
+import Tree
+import BinaryTreeTraversal
 
-#Find BST height
-def height(root):
-    if (root is None):
+def isBalancedBinaryTree(node):
+    return (isBalancedBinaryTreeWrapper(node) >= 0)
+
+#Modified post order traversal      
+#Integrate height 
+def isBalancedBinaryTreeWrapper(node):
+    if node is None:
         return 0
-    x = height(root.left)
-    y = height(root.right)
-    z = max(x,y) + 1
-    print("Node(%d) height=%d" % (root.data,z))
-    return z
-
-#Solution 1
-#Algorithm: Check height of left sub-tree and right-subtree recursively
-#Time complexity: inefficient since nodes are visited more than once
-def CheckBalanced(node):
-    if (node is None):
-        return True
-    print("comparing .... L & R", node.data)
-    if (height(node.getLeft()) - height(node.getRight()) <= 1):
-        #return (CheckBalanced(node.getLeft()) and CheckBalanced(node.getRight())) 
-        return True
-    else:
-        return False
-
-#Solution 2: Modified height to check if nodes are balanced with 3 possible return values (-1, 0 , 1)
-#return -1 if unbalanced
-#return 1 if balanced
-#return 0 if None
-def HeightBalanced(node):
-    if (node is None):
-        return 0
-    x = HeightBalanced(node.left)
-    y = HeightBalanced(node.right)
-    if (x < 0 or y < 0 or abs(x-y) > 1):
+    lheight = isBalancedBinaryTreeWrapper(node.left)
+    rheight = isBalancedBinaryTreeWrapper(node.right)
+    #Kill if height of sub-trees > 1
+    if abs(lheight-rheight) > 1:
         return -1
-    else:
-        return 1
-    
+    #return height of sub-tree
+    return max(lheight,rheight) + 1
 
-import queue
-def levelOrder(root, result):
-    q = queue.Queue()       
-    q.put(root)
-    n = None
-    while not q.empty():
-        n = q.get()  #dequeue FIFO
-        #print(n.getData())
-        result.append(n.getData())
-        if n.left is not None:
-            #print("traversing left ..",n.left.getData())
-            q.put(n.left)
-        if n.right is not None:
-            #print("traversing right ..",n.right.getData())
-            q.put(n.right)  
-                    
-#Initialize Binary Tree
-root = BinaryTree(1)
+
+root = Tree.BinaryTree(5)
 root.insertLeft(2)
-root.insertRight(3)
-root.getLeft().insertLeft(4)
-root.getLeft().insertRight(5)
-root.getRight().insertLeft(6)
-root.getRight().insertRight(7)
-#root.getRight().getRight().insertLeft(8)
-#root.getRight().getRight().getLeft().insertLeft(9)
+root.insertRight(10)
+root.getLeft().insertLeft(1)
+root.getLeft().insertRight(3)
 
-#Traverse
-result = []
-levelOrder(root, result)
-print("LevelOrder traversal: %s" % (result))
-
-#Solution 1    
-#print("Is the binary tree balanced? " , CheckBalanced(root))
-
-#Solution 2
-print("Is the binary tree balanced? ",end="")
-if (HeightBalanced(root) != -1):
-    print("True")
-else:
-    print("False")
-         
+print("Is this tree balanced? %s" % (isBalancedBinaryTree(root)))
