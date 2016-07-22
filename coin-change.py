143c2ae07109ac32730850adab9df26ad8743f6d
Harish Chakravarthy <harishvc@gmail.com>
2016-07-22 07:05:03 -0700

    major rewrite;simplify code and add notes

--- coin-change.py
+++ coin-change.py
@@ -1,37 +1,49 @@
-'''
-Find total #ways to reach a total using given denominations
-'''
+#Find all coin denominations that make a given value
+
+#Solution: Similar to tree traversal 
+#Keep reducing the target value until the value == 0 or negative!
+def AllCoinPermutations(target,denominations,path,level,result):
+	if target == 0:
+		#case 1: valid path
+		result.append(path[:level])
+		return 0
+	elif target < 0:
+		#case 2: invalid path
+		return 
+	else:
+		#case 3: continue to go further
+		for d in denominations:
+			#IMPORTANT: insert current value to path at level
+			path.insert(level,d)
+			#IMPORTANT: level+1
+			AllCoinPermutations(target-d,denominations,path,level+1,result)
+		return  
+
+#Print ONLY permutations with distinct combinations
+def PrintUnique(result):
+	def mysort(self):
+		self.sort()
+		return self
+	#1: sort	
+	result.sort(key=mysort)
+	result2= []
+	for x in result:
+		#2. store unique ones in result2
+		if x not in result2:
+			result2.append(x)
+	for s in result2:
+		print(s)
 
 
-def MyLength(self):
-    return len(self)
-
-
-denominations = [1,2,3,4,5]
-def Combinations(total,result,prefix):
-    global denominations
-    for i in range(len(denominations)):
-        if (denominations[i] <= total):
-            #multiples
-            if ( total % denominations[i] == 0):
-                newcombination = [prefix + [denominations[i] for x in range(int(total/denominations[i]))]]
-                if newcombination not in result:
-                    result += newcombination
-            else:
-                #find combinations
-                new_total = total - denominations[i]
-                tmp = prefix +[denominations[i]]
-                result = Combinations(new_total,result,tmp)
-    #del prefix[:]
-    return result
-
-total = 5
+#OBSERVATION: Order of denominations does not matter
+denominations  = [2,3,1]
+target = 5
 result = []
-Combinations(total,result,[])
-print("Total=%d denominations=%s" % (total,denominations))
-print("# combinations",len(result))
-print("All possible combinations >>>",result)
-result.sort(key=MyLength)
-print("Most optimal solution:",result[0])
+print("All coin denominations that add up to %d" % (target))
+AllCoinPermutations(target,denominations,[],0,result)
 
+#Print ONLY permutations with distinct combinations
+#PrintUnique(result)
 
+for r in result:
+	print(r)
