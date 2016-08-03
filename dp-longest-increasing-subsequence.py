9e1a1c56deabe43ba5b0dcca6e2bdead55a72a23
Harish Chakravarthy <harishvc@gmail.com>
2016-08-02 21:01:54 -0700

    najor rewrite;easy to follow code

--- dp-longest-increasing-subsequence.py
+++ dp-longest-increasing-subsequence.py
@@ -1,104 +1,82 @@
-#Given a list of random numbers. Find length of Longest Increasing Subsequence (LIS) and the sequence.
+#Given random numbers. Find length of Longest Increasing Subsequence (LIS) and the sequence.
 
-'''
-Subsequence is a generalization of substring. Order of values in retained 
-however one of values missing.
-
-OBSERVATION:
-1. Longest Increasing Subsequence (LIS) will have values sorted!
-2. We can keep track of values we have seen (since we don't know the upcoming values)
-3. LIS at each position can be stored in a list
-4. LIS at each position will depend on the NEXT smallest value + 1  
-    - Self Balancing BST can be seached and new value inserted
-'''
-
-#Solution 1: Run two loops
-#http://www.geeksforgeeks.org/dynamic-programming-set-3-longest-increasing-subsequence/
-#Time complexity: O(n^2)
-
-
-#Solution 2: Use Dynamic Programming to take advantage 
-#of information gathered/analyzed so far by using additional storage
-#
 # REFERENCE:
 # 1. https://github.com/mission-peace/interview/blob/master/src/com/interview/array/LongestIncreasingSubSequenceOlogNMethod.java
 # 2. https://www.youtube.com/watch?v=S9oUiVYEq7E
+#
 # NOTES:
 # 1. Keep track of index positions with value low ... high
-# 2. Data Structure:
-#   T = Store start and end index of LIS.
-#   R = Store end index of last LIS, backtrack to get values
-#   maxLength = max length calculated so far
-# 3. Algorithm: New value can have 3 possibilities: low, high or in between
-#     - High:  increment maxLength and add to T
+# 2. Algorithm: New value can have 3 possibilities: low, high or in between
+#     - High:  increment maxLength and add to table, add to result
 #     - Low (lowest):  new T[0] 
 #     - In between: Find the next highest value greater than new value
-#       and replace this value with the new value
-
 
 #Time Complexity: O(nlogn)
 #Space Complexity: O(n)
+
+DEBUG = False
 def LIS(a):
-    T = [0]*len(a)   #LIS at each index
-    R = [-1]*len(a)  #Index positions to backtrack. Default -1
-    maxLength = 0
-    T[0] = 0
-    for i in range(1,len(a)):
-        #Debug
-        #print("i=%s T=%s R=%s maxLength=%d" % (i,T,R,maxLength))
-        #case 1: new value is the largest
-        if a[i] > a[T[maxLength]]:
-            maxLength += 1  #increment ONLY when new value is greater
-            T[maxLength]= i
-            #R[T[maxLength]] = T[maxLength-1]; #store index to backtrack
-            R[i] = T[maxLength-1]; #store index to backtrack
-        #case 2: new value is the smallest
-        elif a[i] < a[T[0]]:
-            T[0] = i
-        #case 3: new value in between small and large
-        else:
-            #maxLength not changing since new value in between
-            #get index of next larger value (compared to new value)  
-            index = findNextLargest(a,T,maxLength,a[i])
-            T[index] = i
-            R[i] = T[index-1];
-        #Debug
-        #print(" i=%s T=%s R=%s maxLength=%d\n" % (i,T,R,maxLength))
-    return(T,R,maxLength)
+	global DEBUG
+	#end index of LIS
+	endIndex = [0]*len(a)
+	#start index of LIS
+	startIndex = [-1]*len(a)
+	#length of LIS
+	lis = 0 
+	endIndex[0] = 0
+	for i in range(1,len(a)):
+		if DEBUG: print("i=%s a[i]=%d endIndex=%s startIndex=%s lis=%d" % (i,a[i],endIndex,startIndex,lis))
+		#case 1: new value is largest
+		if a[i] > a[endIndex[lis]]:
+			lis +=1
+			endIndex[lis] = i
+			startIndex[i] = endIndex[lis -1]
+		#case 2: new value is smallest
+		elif a[i] < a[endIndex[0]]:
+			endIndex[0] = i
+		#case 3: new value in between
+		else:
+			nextLargestIndex = findNextLargest(a,i,a[i],endIndex)
+			endIndex[nextLargestIndex] = i
+			startIndex[i] = endIndex[nextLargestIndex-1]
+		if DEBUG: print("i=%s a[i]=%d endIndex=%s startIndex=%s lis=%d\n" % (i,a[i],endIndex,startIndex,lis))
+	return (startIndex,endIndex,lis)
 
 
-def findLISvalues(a,T,R,maxLength):
-    start = T[maxLength]
-    result = []
-    while start != -1:
-        result.append(a[start])
-        start = R[start]
-    #IMPORTANT:Reverse
-    return result[::-1]
+#Modified Binary Search to find the next value greater than target O(logn)
+def findNextLargest(a,end,target,endIndex):
+	global DEBUG
+	start = 0
+	while start <= end:
+		mid = start + (end-start)//2
+		if a[endIndex[mid]] < target and a[endIndex[mid+1]] > target:
+			if DEBUG: print("new index ...", mid+1)
+			return mid+1
+		elif a[endIndex[mid]] > target:
+			#go left
+			end = mid-1
+		else:
+			#go right
+			start = mid+1
+	return -1 #error
+
+#start from the end index to -1
+def findLISSequence(a,startIndex,endIndex,lis):
+	end = endIndex[lis]
+	result = []
+	#Keep going LEFT to generate the sequence
+	while end != -1:
+		result.append(a[end])
+		#go left
+		end = startIndex[end]
+	#IMPORTANT:Reverse the sequence
+	return result[::-1]
 
 
-#Modified Binary Search to find the next value greater than target O(nlogn)
-def findNextLargest(a,T,end,target):
-    start = 0
-    #IMPORTANT: start <= end , since there is one value!!!
-    while start <= end:
-        mid = start + (end-start)//2
-        if a[T[mid]] < target and a[T[mid+1]] > target:
-            return mid+1
-        elif a[T[mid]] < target :
-            #go right
-            start = mid+1
-        else:
-            #go left
-            end = mid-1
-    return -1 #ERROR
 
-
-a= [3,4,-1,5,8,2,3,12,7,9,10]
-T,R,maxLength = LIS(a)
-result = findLISvalues(a,T,R,maxLength)
-print("Input >>>", a)
-print("Max LIS Length=", maxLength+1)
-print("LIS = ", result  )
-
-
+a= [10, 22, 9, 33, 21, 50, 41, 60]
+startIndex,endIndex,lis = LIS(a)
+print("input >> ",a)
+#IMPORTANT: lis+1
+print("LIS length=",lis+1)
+print("LIS=",findLISSequence(a,startIndex,endIndex,lis))
