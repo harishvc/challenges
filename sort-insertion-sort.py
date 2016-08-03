842de970c22c6c6906f6ec4cf49998d13fc416fb
Harish Chakravarthy <harishvc@gmail.com>
2016-08-02 21:01:08 -0700

    major rewrite;clean up and easy to scan

--- sort-insertion-sort.py
+++ sort-insertion-sort.py
@@ -1,40 +1,38 @@
 #Insertion Sort
 
-''''
-ALGORITHM:
-1. Iterate through the items in the list 
-2. If the value is greater than the next
-    a. shift values
-    b. go back and shift values 
-The sub-list is sorted at the end of each pass!!!
-
+'''
 NOTES:
 1. Insertion sort is simple (no recursion)
 2. For small lists, insertion is generally faster than a comparably implemented quicksort or mergesort
-3. In-place sort
+3. In-place sort & STABLE
 4. Insertion sort maintains a sorted sub-list
 5. Each new value is inserted inside a sorted sublist
 6. Shift operation requires approximately a third of the processing work of an exchange since only one assignment is performed
 7. Time complexity: O(nlogn) - consistent, NO assumptions are made here!!!
 8. Space complexity: Best: O(n), Average O(n^2)
 
-
-REFERENCE:
-http://interactivepython.org/runestone/static/pythonds/SortSearch/TheInsertionSort.html
+ALGORITHM OBESERVATION:
+1. Values are sorted from left to right
+2. At iteration i, all values from 0 ... i+1 are sorted
+3. Inner while loop will not get executed if i+1 > i (already sorted)
+4. Insertion sort makes less comparisons if list is sorted!
 '''
+
+def insertionSort(a):
+	#IMPORTANT: exclude last value; since we are checking i & i+1
+	end = len(a) -1 
+	#go right!
+	for right in range(0,end):	
+		left = right
+		#go left!
+		#IMPORTANT: If values from a[0] ... a[i+1] are sorted, this loop will NOT execute
+		while left >= 0 and a[left] > a[left+1]:
+			a[left],a[left+1] = a[left+1],a[left]
+			left -= 1
 
 
 import random
-def insertionSort(input):
-	inputLength = len(input)-1  #ignore last value
-	for next in range(inputLength):
-		i = next
-		#next value < sort again!
-		while i >= 0 and input[i+1] < input[i]:	
-			input[i+1],input[i] = input[i],input[i+1]
-			i -=1
-
-alist = [54,26,93,17,77,31,44,55,20]
+alist = [5,4,3,2,1,1,3,2,1]
 random.shuffle(alist)
 print("input >>>", alist)
 insertionSort(alist)
