9cebe4a5c09b89ea156d900b60fde0b3806f5fa3
Harish Chakravarthy <harishvc@gmail.com>
2016-08-03 22:20:21 -0700

    Add examples

--- python-intro3.py
+++ python-intro3.py
@@ -27,8 +27,8 @@ print(len(y))      # 1
 x.append(5)        #append 5 at the end
 
 ###LIST & DELETE
-del x[0:1]         #delete elements in index 0, similar to pop() except del has no return
-del x[:]           #delete ALL elements inside x, similar to pop() except del has no return
+del x[0:1]         #delete elements in index 0
+del x[:]           #delete ALL elements inside x
 
 ###SETS
 s = set([1,2,3,4])
@@ -37,6 +37,8 @@ s.discard(3)
 print(s) #[1,2,4]
 
 
+#a.sort() Inplace sort
+#sorted() returns a new list, works for lists and dict
 
 ####Sort List
 lucky = ['22', '11', '31']
@@ -55,24 +57,34 @@ print(xyz)
 print("~~~~~~~~~")
 
 #####List of lists
+#Question: Reverse sort by lucky number
 people2 = [
     ['Harish', 'Chakravarthy', 22],  #first name, last name, lucky number
     ['Jon', 'Doe', 27],
     ['Foo', 'Bar', 18],
 ]
 print(people2)
+#Solution 1
 people2.sort(key=lambda x: x[2],reverse=True)
 print ("sorted by lucky number in reverse")
 print(people2)
 print("~~~~~~~~~")
-##
+#Solution 2 
+from operator import itemgetter            
+people3 = sorted(people2,key=itemgetter(2),reverse=True)
+print(people3)
+print("~~~~~~~~~")
+#
+#
+#Question: Sort by key1 and key2
 print(input)
 input = [[1,3],[4,6],[10,15],[1,2],[2,9]]
 #sort by key1 and then key2
 input.sort(key=lambda x:(x[0],x[1]))
 print("sorted input ===>", input)
-
-####List of dictionaries
+#
+###List of dictionaries
+#Question: sort by last name
 people3 = [
     {'FirstName':'Harish','LastName':'Chakravarthy','Lucky':22},
     {'FirstName':'Jon', 'LastName':'Doe', 'Lucky':27},
@@ -82,4 +94,35 @@ print(people3)
 people3.sort(key=lambda x: x['LastName'],reverse=True)
 print ("sorted by last name in reverse")
 print(people3)
+
+#####Tuples
+#Question: Given height,base,width as tuples. Sort by base
+from operator import itemgetter            
+a = [(1, 2, 3), (2, 4, 3), (8, 1, 4)]
+b = sorted(a,key=itemgetter(1))
+print(b)  #[(8, 1, 4), (1, 2, 3), (2, 4, 3)]
+print("~~~~~~~~~")
+#
+#
+#
+#
+######Dictionaries
+a = {}
+a['hello'] = 5
+a['world'] = 4
+a['aa'] = 1
+a['abc'] = 10
+#Question: Sort by key
+for k in sorted(a):
+	print(k,a[k])
+
+'''
+aa 1
+abc 10
+hello 5
+world 4
+'''
+#Question: Sort by value
+for k in sorted(a, key=a.get):
+	print(k,a[k])
 
