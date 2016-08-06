a532d2de4cff3aabb4ca5ddacc8bffe03791e08d
Harish Chakravarthy <harishvc@gmail.com>
2016-08-06 07:38:49 -0700

    rewrite; add :notes:

--- matrix-spiral-print.py
+++ matrix-spiral-print.py
@@ -1,52 +1,44 @@
+#Print Spiral Matrix 
+
 '''
-Question: Given a two dimensional matrix print inside out (spiral order)
+OBSERVATION:
+1. Spiral print starts at index 0,0
+2. Direction = go right > got down > go left > go top
+3. Have 4 pointers to indicate the start index in each direction 
 '''
 
+a = [[1,2,3,4,5], [6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]
 
-def PrintMatrix(Table):
-    for row in range(0,len(Table)):
-        for col in range(0,len(Table[row])):
-            print("%d" % (Table[row][col]),end=" ")
-        print("")
+rows = len(a)
+cols = len(a[0])
 
+topR = 0           #index to start row
+bottomR = rows-1   #index to start bottom row
+leftC = 0          #index to start left col 
+rightC = cols-1    #index to start right col
 
-#References
-#1. http://www.geeksforgeeks.org/print-a-given-matrix-in-spiral-form/
-#2. http://stackoverflow.com/questions/726756/print-two-dimensional-array-in-spiral-order
-def SpiralBind(rowEnd,colEnd,table):
-    rowStart = 0
-    colStart = 0
-    result = []
-    while (rowStart < rowEnd and colStart < colEnd):
-        #top row
-        for i in range(colStart,colEnd):
-            result.append(table[rowStart][i])
-        rowStart +=1
-        #right most col
-        for i in range(rowStart,rowEnd):
-            result.append(table[i][rowEnd-1])
-        colEnd -=1
-        #last row
-        if(rowStart < rowEnd):
-            for i in range(colStart,colEnd)[::-1]:
-                result.append(table[rowEnd-1][i])
-            rowEnd -=1
-        #left most col
-        if(colStart < colEnd):
-            for i in range(rowStart,rowEnd)[::-1]:
-                result.append(table[i][colStart])
-            colStart += 1
-    return result
-        
-        
+while topR < bottomR and leftC < rightC:
+	#top row , left to right
+	for i in range(leftC,rightC+1):
+		print(a[topR][i],end=" ")
+	topR +=1
 
+	#far right col, top to bottom
+	for i in range(topR,bottomR+1):
+		print(a[i][rightC],end=" ")
+	rightC -= 1
 
-table = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
-print("2x2 matrix")
-PrintMatrix(table)
-   
-result = SpiralBind(4,4,table)
-print("\nspiral bind ...")
-print(result)
-answer = [1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]
-assert result == answer, "invalid!!!!"
+	#bottom row, right to left
+	#IMPORTANT: -1 , going right to left
+	for i in range(rightC,leftC-1,-1):
+		print(a[bottomR][i],end=" ")
+	bottomR -=1
+
+	#left col, bottom to top
+	#IMPORTANT: -1, going from bottom to top
+	for i in range(bottomR,topR-1,-1):
+		print(a[i][leftC],end=" ")
+	leftC +=1
+
+print()
+#1 2 3 4 5 10 15 20 19 18 17 16 11 6 7 8 9 14 13 12
