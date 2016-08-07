4318d57caa175f82350afb4bceca7af9aa5d22a8
Harish Chakravarthy <harishvc@gmail.com>
2016-08-07 13:40:53 -0700

    major rewrite;simplify code :thumbsup:

--- binary-search-tree-closest-value.py
+++ binary-search-tree-closest-value.py
@@ -1,5 +1,8 @@
 '''
 Question: In a BST given target find a value close to the target
+
+REFERENCE:
+1. http://stackoverflow.com/questions/6209325/how-to-find-the-closest-element-to-a-given-key-value-in-a-binary-search-tree
 '''
 
 import sys
@@ -10,30 +13,24 @@ import BST
 root = BST.BSTNode(4)
 input = [3,2,1,5,10,7,6,9,12,15]
 for x in input:
-    BST.insertNode(root, BST.BSTNode(x))
+    BST.insert(root, BST.BSTNode(x))
 
 
-#http://stackoverflow.com/questions/6209325/how-to-find-the-closest-element-to-a-given-key-value-in-a-binary-search-tree
-def ClosestValue(node,target,result):
-    #End?
-    if(node is None):
-        return result
-    
-    #Found new value?
-    if(node.data == target):
-        return node.data
-    if(result is None or  abs(target-node.data) < abs(target-result)):
-       #found new value!
-       result = node.data
-    
-    #Next path?   
-    if(node.data < target):
-        #go right
-        return(ClosestValue(node.right,target,result))
-    else:
-        #go left
-        return(ClosestValue(node.left,target,result))
+def  ClosestValue(node,target,result):
+	while node:
+		#find closest node
+		if node.data == target:
+			return node.data
+		elif abs(target-node.data) < abs(target-result):
+			#new closest node
+			result = node.data
+		#go left or right?
+		if node.data > target:
+			node = node.left
+		else:
+			node = node.right
+	return result
 
-target = 11
-print("Value close to ", target, " = ", ClosestValue(root,target,-1))
+target = 8
+print("Value close to ", target, " = ", ClosestValue(root,target,root.data))
 
