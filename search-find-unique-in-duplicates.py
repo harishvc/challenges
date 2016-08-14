60e654ff91a64bf6b70a33804faa1ad10a8739b7
Harish Chakravarthy <harishvc@gmail.com>
2016-08-14 13:41:27 -0700

    add algorithm to find a unique value in duplicates

---
+++ search-find-unique-in-duplicates.py
@@ -0,0 +1,13 @@
+'''
+Find the element that occurs only once in a given set of integers while all the other numbers occur thrice.
+Example -  
+Input : 3 3 3 4
+Output: 4
+
+Input : 5 5 4 8 4 5 8 9 4 8
+Output: 9
+
+
+http://www.ideserve.co.in/learn/find-the-element-that-appears-once-in-an-array
+'''
+
