6a3d5b5158fa4ad9d74be76011cf8825bd53a9ad
Harish Chakravarthy <harishvc@gmail.com>
2016-08-20 12:46:58 -0700

    major rewrite;easy to scan and follow

--- string-longest-palindrome-substring.py
+++ string-longest-palindrome-substring.py
@@ -1,17 +1,16 @@
 #Find the longest palindromic substring 
 
 '''
-NOTES:
-References: 
-1. http://difusal.blogspot.com/2014/08/manachers-algorithm-longest-palindromic.html
-2. http://articles.leetcode.com/longest-palindromic-substring-part-ii/
-
-Manacher’s Algorithm
-Take advantage of the palindrome’s symmetric property and avoid some of the unnecessary computations.
-1. Create a new string (T) by adding # on the left and right side of each character in the string  
+Manacher’s Algorithm:
+1. Take advantage of the palindrome’s symmetric property and avoid some of the unnecessary computations
+    - Create a new string (T) by adding # on the left and right side of each character in the string  
     - new string (T) length is odd
-2. Consider each character in T to be center of a palindrome. How long is the palindrome? store in new list P
-3. Using the maximum value of list P the longest palindromic substring can be constructed!
+2. Consider each character in T to be center of a palindrome and expand. Find new center (C), end of palindrome (R) & maxLength at each index
+3. If current index (i) < R, see if we can take advantage of already claculated value
+4. Iterate maxLength and find the length of palindrome and reconstruct the characters
+
+References: 
+1. https://www.youtube.com/watch?v=nbTSfrEfo6M&spfreload=5
 '''
 
 #Pre-processing
@@ -19,40 +18,62 @@ def pad(a,mypad):
 	result = mypad
 	for x in a:
 		result += x + mypad
-	return result
+	#start $ end @ for code simplicity	
+	return "$"+result+"@"
 
 '''
 Whenever a palindrome centered at i expands past the right bound of the palindrome 
 centered at C (in other words, if it expands past R), C is assigned the value of i 
 and R is updated according to the content of P[i] and i itself.
 '''
+
+
+
 #Time & Space complexity: O(n)
-def ManacherAlgorithm(a,mypad):
-	#step 1: pre-processing
+def ManacherAlgorithm(a,mypad):	
+	#
+	#Preprocessing
 	T=pad(a,mypad)
-	#step 2: create list to store max length of palindrome at each index position
-	p = [0]*len(T)
-	C,R = 0,0
-	maxCenterIndex = 1
+	maxLength = [0]*len(T)
+	#center index of longest palindrome calculated so far
+	C = 0
+	#right index of palindrome calculated so far	
+	R = 0
+	
+	#
+	#Find the length of the longest palindrome that each index
 	for i in range(1,len(T)-1):
-		ii = 2 * C - i
-        #optimization!!!
-		p[i] = min(R - i, p[ii]) if (R > i)  else 0
-		#expand the palindrome
-		while (i+1+p[i] < len(T) and i-1-p[i] >=0 and T[i + 1 + p[i]] == T[i - 1 - p[i]]):
-			p[i] += 1
-		#adjust center if i + p[i] > R	
-		if (i + p[i] > R):
+		#Step 1: Take advantage of the work do so far
+		# i < R, we can already seen T[i]
+		if i < R:
+			#IMPORTANT: length of mirror 
+			mirror = 2*C - i
+			#IMPORTANT: min length of palindrome at index i
+			maxLength[i] = min(maxLength[mirror],R-i)
+		#Step 2: Expand with i as the center of both sides
+		while T[i + (maxLength[i]+ 1)] == T[i - (maxLength[i]+1)]:
+			maxLength[i] +=1
+		#Step 3: Find new C and R
+		if (maxLength[i] + i > R):
 			C = i
-			R = i + p[i]
-    	#Keep track of the index with max value
-		if p[i] > p[maxCenterIndex]:
-			maxCenterIndex = i
+			R = maxLength[i] +i
 
-	return T[maxCenterIndex-p[maxCenterIndex]:maxCenterIndex+p[maxCenterIndex]].replace(mypad,"")
+	#Find the longest length (from the center)
+	#Construct the left and right of the palindrome 
+	center = 0
+	length = 0
+	for i in range(len(maxLength)):
+		if maxLength[i] > length:
+			length = maxLength[i]
+			center = i
+	start = T[center-length:center]
+	end = T[center:center+length]
+	t = start+end
+	#replace mypad		
+	return t.replace(mypad,"")
 
 
-a ="abaccddccefe"
-#a ="cfceg"
+a ="abaccddccefeG"
 mypad="#"
-print(a, " >>>> ", ManacherAlgorithm(a,mypad))
+print("input >>>", a)
+print("Longest Palindrome >>> ", ManacherAlgorithm(a,mypad))
