7f7526f7f0fa9ac070518869966f4a1f1dde3408
Harish Chakravarthy <harishvc@gmail.com>
2016-08-27 07:55:39 -0700

    clean up; add notes

--- mylib/MinHeap.py
+++ mylib/MinHeap.py
@@ -1,61 +1,89 @@
-#Min Heap Implemenation
+
+#Min Implemention & Heap Sort
 
 class MinHeap:
 	def __init__(self):
-		self.values = [-1] #starting from index=1
+		self.data = [0] # index position 0 = 0 (SIMPLICITY !!!!)
 		self.size = 0
 
-	def heapSize(self):
-		return self.size
+	def insert(self,data):
+		self.data.append(data)
+		self.size +=1 
+		self.percolateUP()
 
-	def parentIndex(self,index):
-		return index//2
+	def percolateUP(self):
+		current = self.size
+		#IMPORTANT: current//2 > 0  since start index=1 
+		while current//2 > 0:
+			parent = current//2
+			if self.data[current] < self.data[parent]:
+				self.data[current],self.data[parent] = self.data[parent],self.data[current]
+				current = parent
+			else:
+				#IMPORTANT: heap property is satisfied
+				break
 
-	def leftChildIndex(self,index):
-		return index*2
+	def myprint(self):
+		print(self.data)
 
-	def rightChildIndex(self,index):
-		return index*2+1
 
-	def minChildIndex(self,index):
-		if index*2+1 > self.size: #no right child
+	def delete(self):
+		myreturn = self.data[1] #replace with last value
+		self.data[1] = self.data[self.size]
+		self.size -= 1
+		self.data.pop() #remove last value
+		self.percolateDOWN()
+		return myreturn
+
+	def getMinChild(self,index):
+		#IMPORTANT: NO right child node
+		if 2*index+1 > self.size:
 			return index*2
 		else:
-			if self.values[index*2] < self.values[index*2+1]:
+			if self.data[index*2] < self.data[index*2+1]:
 				return index*2
 			else:
 				return index*2+1
 
-	def insertNode(self,K):
-		self.values.append(K)
-		self.size += 1
-		self.percolateUP(self.size)
+	def percolateDOWN(self):
+		node = 1
+		#IMPORTANT: node * 2 <= self.size , covers both left and right child nodes 
+		while node * 2 <= self.size:
+			minChildNodeIndex = self.getMinChild(node)
+			if self.data[node] > self.data[minChildNodeIndex]:
+				self.data[node],self.data[minChildNodeIndex] = self.data[minChildNodeIndex],self.data[node]
+				node = minChildNodeIndex
+			else:
+				#IMPORTANT: heap property is satisfied
+				break
 
-	def percolateUP(self,i):
-		while i//2 > 0: #IMPORTANT: > 0 since root is at index 1
-			parentIndex = self.parentIndex(i)
-			if self.values[parentIndex] > self.values[i]:
-				tmp  = self.values[parentIndex]
-				self.values[parentIndex]  =  self.values[i]
-				self.values[i] = tmp
-			i = i//2 #keep going up, each node has ONE parent
 
-	def deleteNode(self):
-		tmp = self.values[1]  #IMPORTANT: start index=1
-		self.values[1] = self.values[self.size]
-		self.size -= 1
-		self.values.pop()
-		self.percolateDOWN(1) #IMPORTANT: index=1
-		return tmp #IMPORTANT: return value
+a = [5,2,1,3,4]
+h = MinHeap()
+for i in a:
+	h.insert(i)
 
-	def percolateDOWN(self,i):
-		while i*2 <= self.size:
-			minChildIndex = self.minChildIndex(i)
-			if self.values[i] > self.values[minChildIndex]:
-				tmp = self.values[i]
-				self.values[i] = self.values[minChildIndex]
-				self.values[minChildIndex] = tmp
-			i = minChildIndex #IMPORTANT: ONLY visit branch with min value
+print(h.delete())  #1
+print(h.delete())  #2
+h.insert(1)       
+h.insert(2)
+print(h.delete())  #1 
+print(h.delete())  #2
 
-	def printHeap(self):
-		print(self.values[1:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
