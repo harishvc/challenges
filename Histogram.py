9ac54bfd13dbfc65ca9e13c544264b2223a61867
Harish Chakravarthy <harishvc@gmail.com>
2016-08-29 20:30:05 -0700

    major rewrite;clean up;easy to follow :boom:

--- Histogram.py
+++ Histogram.py
@@ -4,59 +4,71 @@
 NOTES/OBSERVATION
 1. Store index positions in stack
 2. Stack has increasing values
-3. Ignore equal values (value at top of stack == new value)
-4. Pop values from stack when new value is <  value at to of stack
-    - find area with appropriate width
-5. After completing iteration, start popping values from the stack and find area
+3. If we arrive at a bar with height greater than top of stack, 
+   this may be the START of a max area rectangle so add to stack 
+4. If we arrive at a bar with height smaller stack top, close ALL pending sub problems. 
+   Calculate area at each bar - height is the popped element height 
+   and width is how much we have traversed from the previous smaller 
+   height that's why we store index in the stack 
+5. At the end  calculate areas for all remaining sub problems
 
 Reference
 1. http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+2. https://prismoskills.appspot.com/lessons/Arrays/Largest_rectangle_under_histogram.jsp
+
 '''
 
 
-def maxArea(a):
-	values = [] #store index positions with increasing value
-	size = 0
+def findnewArea(mystack,mystack_size,height,i):
+	left_boundry = 0
+	right_boundry = 0
 	area = 0
-	for i in range(len(a)):
-		#case 1: empty
-		if size == 0:
-			values.append(i)
-			size += 1
-		#case 2: new value > 
-		elif a[values[-1]] < a[i]:
-			values.append(i)
-			size +=1
-		#case 3: same value; continue	
-		elif a[values[-1]] == a[i]:
-			continue
-		#case 4: new value < 
+	#Find left & right boundry
+	if mystack_size == 0:
+		left_boundry = 0
+		right_boundry = i
+	else:
+		left_boundry = mystack[-1]
+		right_boundry = i -1
+	area = (right_boundry - left_boundry) * height
+	#print("area @ %d =%d  lb=%d rb=%d" % (height,area,left_boundry,right_boundry))
+	return area
+
+
+def maxArea(a):
+	mystack = []
+	mystack_size = 0 #initialize
+	mystack.append(0) #initialize
+	mystack_size +=1
+	area = 0
+	for i in range(1,len(a)):
+		if a[i] > a[mystack[-1]]:
+			mystack.append(i)
+			mystack_size +=1
+			#print(">>> mystack", mystack)
 		else:
-			assert a[values[-1]] > a[i], "logic error"
-			while True:
-				#start popping values
-				height = values.pop()
-				size -= 1
-				width = i #default width
-				if size > 0:
-					#IMPORTANT: new width!
-					width = i - values[-1] -1
-				area = max(area, a[height]*width)
-				#IMPORTANT: Handle empty stack
-				if size == 0 or a[values[-1]] <= a[i]:
-					break
-			#IMPORTANT: Append new value		
-			values.append(i)
-			size +=1
+			#pop all values >= new value - including equal values
+			while mystack_size > 0 and a[mystack[-1]] >= a[i]: 
+				iheight = mystack.pop()
+				mystack_size -= 1
+				new_area = findnewArea(mystack,mystack_size,a[iheight],i)
+				#Update new area
+				area = max(area,new_area)
+			mystack.append(i)
+			mystack_size +=1
 	#IMPORTANT: pop rest of the stack - values are increasing!
 	width = 0
-	while values:
-		width +=1
-		height = values.pop()
-	area = max(area, a[height]*width)
+	while mystack:
+		height = mystack.pop()
+		width +=1 ##values greater than a[height]!
+		new_area = a[height]*width 
+		#print("area @ %d =%d" % (a[height],new_area))
+		area = max(area, new_area)
+
 	return area	
 
 
-values = [[2,4,2,1], [6, 2, 5, 4, 5, 2, 6], [3,2, 4, 6,8], [3,6,3,6,5,2]]
+#values = [[2,4,2,1], [6, 2, 5, 4, 5, 2, 6], [3,2, 4, 6,8], [3,6,3,6,5,2], [10, 40, 30, 70, 10, 30, 60]]
+values = [[10, 40, 30, 70, 10, 30, 60]]
 for a in values:
 	print("%s max area=%d" % (a,maxArea(a)))
