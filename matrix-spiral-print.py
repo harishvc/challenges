d4db717c833c63e15b11d2079f2d2212695cedeb
Harish Chakravarthy <harishvc@gmail.com>
2016-09-12 20:49:47 -0700

    Major rewrite; expanded notes

--- matrix-spiral-print.py
+++ matrix-spiral-print.py
@@ -1,44 +1,59 @@
-#Print Spiral Matrix 
+#Spiral print matrix
 
 '''
-OBSERVATION:
-1. Spiral print starts at index 0,0
-2. Direction = go right > got down > go left > go top
-3. Have 4 pointers to indicate the start index in each direction 
+input >>>
+    1	2	3	4	5
+    6	7	8	9	10
+    11	12	13	14	15
+    16	17	18	19	20
+
+NOTES:
+1. Use 4 pointers to mark start end end of rows and columns
+   rowStart = 0
+   rowEnd = 4
+   colStart = 0
+   colEnd = 4
+2. Traverse until index pass each other
+    - left 2 right
+    - top 2 bottom
+    - right 2 left
+    - bottom 2 top   
 '''
+
+def MatrixSpiral(a):
+	rowStart = 0
+	colStart = 0
+	#IMPORTANT: -1
+	rowEnd = len(a) -1
+	#IMPORTANT: -1
+	colEnd =len(a[0]) -1
+	#Exit condition: until pointers pass each other! 
+	while rowStart <= rowEnd and colStart <= colEnd:
+		#left 2 right
+		for i in range(colStart,colEnd+1):
+			print(a[rowStart][i], end=" ")
+		rowStart += 1
+
+		#top to bottom
+		for i in range(rowStart,rowEnd+1):
+			print(a[i][colEnd], end=" ")
+		colEnd -= 1
+
+		#right 2 left
+		for i in range(colEnd,colStart-1,-1):
+			print(a[rowEnd][i], end=" ")
+		rowEnd -=1
+
+		#bottom to top
+		for i in range(rowEnd,rowStart-1,-1):
+			print(a[i][colStart], end=" ")
+		colStart +=1
+
+
 
 a = [[1,2,3,4,5], [6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]
+#Result
+#1 2 3 4 5 10 15 20 19 18 17 16 11 6 7 8 9 14 13 12 
 
-rows = len(a)
-cols = len(a[0])
-
-topR = 0           #index to start row
-bottomR = rows-1   #index to start bottom row
-leftC = 0          #index to start left col 
-rightC = cols-1    #index to start right col
-
-while topR < bottomR and leftC < rightC:
-	#top row , left to right
-	for i in range(leftC,rightC+1):
-		print(a[topR][i],end=" ")
-	topR +=1
-
-	#far right col, top to bottom
-	for i in range(topR,bottomR+1):
-		print(a[i][rightC],end=" ")
-	rightC -= 1
-
-	#bottom row, right to left
-	#IMPORTANT: -1 , going right to left
-	for i in range(rightC,leftC-1,-1):
-		print(a[bottomR][i],end=" ")
-	bottomR -=1
-
-	#left col, bottom to top
-	#IMPORTANT: -1, going from bottom to top
-	for i in range(bottomR,topR-1,-1):
-		print(a[i][leftC],end=" ")
-	leftC +=1
-
-print()
-#1 2 3 4 5 10 15 20 19 18 17 16 11 6 7 8 9 14 13 12
+MatrixSpiral(a)
+print("")
