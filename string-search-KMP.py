485241a9c63f96c35b8846772018943d5f063cc4
Harish Chakravarthy <harishvc@gmail.com>
2016-09-16 19:49:54 -0700

    Add brut force + notes

--- string-search-KMP.py
+++ string-search-KMP.py
@@ -1,3 +1,22 @@
+#Search for a pattern in text
+
+#Solution 1:
+#Brut force: Time Complexity: O(n * m) 
+# n = # characters in text , m = #characters in pattern
+def findPattern(text,pattern):
+    for t in range(0,len(text)):
+        t2 = t
+        for p in range(0,len(pattern)):
+            if text[t2] == pattern[p]:
+                t2 +=1
+                if  p == len(pattern) -1:
+                    print("match at index ...", t)
+                    break
+            else:
+                break
+
+
+#Solution 2: KMP
 '''
 Implement KMP (Knuth Morris Pratt) algorithm to search a pattern in a long string
 
@@ -23,6 +42,7 @@ pattern = snape , longest proper suffix = nape,ape,pe,e
 the values will be 0 - start from beginning if there is no match
 '''
 
+
 #O(m) - m is the length of the pattern
 def MaxShift(pattern):
     size = len(pattern)
@@ -39,6 +59,7 @@ def MaxShift(pattern):
             #IMPORTANT: move back j
             j = maxShift[j-1]
         else:
+            assert j == 0, "logic error"
             #j == 0
             i += 1    
     #print(maxShift)        
@@ -57,7 +78,10 @@ def KMP(text,pattern):
             #ENTIRE PATTERN MATCH!!!
             if(p >= len(pattern)):
                 yield t-p
-                #IMPORTANT: p is NOT reset to 0 to find next occurance 
+                #IMPORTANT: 
+                # 1. p is NOT reset to 0
+                # 2. This logic handles recurring occurances
+                # 3. Example: text = GGCACACATG, pattern = CAC apprears twice 
                 p = maxShift[p-1] #shift pattern index
         elif p != 0:
             #IMPORTANT: Get shift position based on last character 
@@ -68,10 +92,12 @@ def KMP(text,pattern):
             t +=1
 
 
-text = "abcxabcdabcdabcyqeeabcdabcy"
-pattern = "abcdabcy"
+text = "GGCACACATG"
+pattern = "CACA"
+
 print("input >>>", text)
 print("pattern >>>", pattern)
 result = KMP(text,pattern)
 for x in result:
     print("pattern found at start position >>>", x)
+
