40a4367a6bcb0ac413c6c8af7c822326bbd8cd58
Harish Chakravarthy <harishvc@gmail.com>
2016-09-28 07:14:17 -0700

    major rewrite, easy to follow

--- dp-egg-drop.py
+++ dp-egg-drop.py
@@ -1,81 +1,68 @@
 #Given some number of floors and some number of eggs, what is the minimum number 
-#of attempts it will take to find out from which floor egg will break.
+#of attempts it will take to find out from which floor egg will break in the worst case.
 
 
 '''
 NOTES:
-1. if egg break at 
-    - floor 0 we found the floor
-    - floor > 0 , we need to find the "critical floor" BELOW with ONE LESS egg
-2. if egg does not break
-    - we need to find the "critical floor" ABOVE with N eggs
-3. if #eggs = 0:
-	- return #floors traversed
-4. if #floor < #eggs:
-	- return #floors
+1. There are 2 choices at each step
+    - egg breaks
+    - egg does not break
+2. If egg breaks, you have one less egg and you move DOWN (current_floor - 1)
+3. If egg does not break, #eggs does not change and you move UP to a new floor (total #floors - current)
+4. Find max attempts (worst case) for each #2 #3
+5. Find min of all possible attempts
 
 REFERENCE:
 1. https://www.youtube.com/watch?v=3hcaVyX00_4
-
+2. https://prismoskills.appspot.com/lessons/Dynamic_Programming/Chapter_11_-_Egg_dropping_puzzle.jsp
+3. http://www.geeksforgeeks.org/dynamic-programming-set-11-egg-dropping-puzzle/
+4. https://github.com/harishvc/challenges/blob/master/images/egg-drop-recursion.jpg
 '''
 
-#Suppose that we wish to know which stories in a 36-story building are safe to drop eggs from, and which will cause the eggs to break on landing. 
-#The problem is not actually to find the critical floor, but merely to decide floors from which eggs should be dropped so that total number of trials are minimized
-#
-
-
 import sys
+def eggDropRecursive(eggs,floors):
+	#IMPORTANT: Exit condition
+	if floors == 0 or eggs == 1:
+		return floors
+	result = sys.maxsize
+	#IMPORTANT: start=1, end=floor+1
+	for current_floor in range(1,floors+1):
+		#egg breaks: 1 less egg and current_floor-1 floors to check
+		left = eggDropRecursive(eggs-1,current_floor-1)
+		#egg does not break: # eggs doesn't change, floors-current_floor to check
+		right = eggDropRecursive(eggs,floors - current_floor)
+		#find max to handle worst case
+		worst_case_attempts = max(left,right) + 1
+		#final result is minimum of all worst cases!
+		result = min(result,worst_case_attempts)
+	return result
 
-#Recursive solution
-def eggDropRecursive(n,k):
-  if k <=1:
-  	return k
-  if n == 1:
-  	return k
-  count = sys.maxsize
-  for i in range(1,k):
-  	left = 0
-  	right = 0
-  	iMax = 0
-  	#print("i=%d left=%d right=%d  iMax=%d count=%d" % (i,left,right,iMax,min(count,iMax)))
-  	left  = eggDropRecursive(n-1,i-1)
-  	right = eggDropRecursive(n,k-i)
-  	iMax = max(left,right)
-  	#print("\ti=%d left=%d right=%d  iMax=%d count=%d" % (i,left,right,iMax,min(count,iMax)+1))
-  	count = min(count,iMax)
-  return count+1
 
-#Dynamic Porogramming
-def eggDropDP(eggsCount,floorsCount):
-	#DP Table
-	#rows = #eggs
-	#cols = #floors
-	#Add +1 to rows and cols to simplify code
-	minAttempts = [[sys.maxsize for i in range(floorsCount+1)] for j in range(eggsCount+1)]
-
-	for i in range(1,eggsCount+1):
-		for j in range(1,floorsCount+1):
-			#case 1: #eggs = 1, #attempts=#eggs
-			if i == 1:
-				minAttempts[i][j] = j
-			#case 2: #stairs <= #eggs	
-			elif j<= i:
-				minAttempts[i][j] = j
-			#case 3:
+#Time Complexity: O(ef^2) , e = #eggs  f= #floors
+#Auxiliary Space: O(ef)
+def eggDropDP(eggs,floors):
+	table = [[sys.maxsize for i in range(0,floors+1)] for j in range(0,eggs+1)]
+	for e in range(1,eggs+1):
+		for f in range(1, floors+1):
+			#case 1: when egg =1
+			if e == 1:
+				table[e][f] = f
+			#case 2: floor <= egg
+			elif f <= e:
+				table[e][f] = f
+			#case 3: calculate from prior values
 			else:
-				#tmp = table[i-1][j-1]
-				for x in range(1,j):
-					tmp = 1 + max(minAttempts[eggsCount-1][x-1],minAttempts[eggsCount][j-x])
-					minAttempts[i][j] = min(minAttempts[i][j],tmp)
+				for i in range(1,f):
+					left = table[e-1][i-1]
+					right = table[e][f-i]
+					worst_case_attempts = max(left,right) + 1
+					table[e][f] = min(table[e][f],worst_case_attempts)
+	#print(table)
+	return table[eggs][floors]
 
-	#print(minAttempts)				
-					
-	return minAttempts[i][j]
-					
 
-n =2  #eggs count
-k =6  #floors count
+floors = 10
+eggs = 2
+print("Minimum attempts in worst case with %d eggs from %d floors = %d" % (eggs,floors,eggDropDP(eggs,floors)))
 
-#print(eggDropRecursive(n,k))
-
-print(eggDropDP(n,k))
+	
