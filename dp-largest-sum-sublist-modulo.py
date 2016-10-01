6d874c890dbb182411002a8f5185abb9a8946f84
Harish Chakravarthy <harishvc@gmail.com>
2016-10-01 09:04:14 -0700

    fix typo

--- dp-largest-sum-sublist-modulo.py
+++ dp-largest-sum-sublist-modulo.py
@@ -1,8 +1,8 @@
-#Find #occurances of the largest sum contiguous subarray of modulo K
+#Find #occurrences of the largest sum contiguous subarray of modulo K
 
 
 '''
-Modify Kadane algorithm to keep track of #occurences.
+Modify Kadane algorithm to keep track of #occurrence
 This solution handles +ve and -ve values. 
 This solution can be easily modified to handle  all -ve values
 
@@ -16,7 +16,7 @@ Reference:
 def maxContiguousSum(a,K):
 	sum_so_far =0
 	max_sum = 0
-	count = {} #keep track of occurences
+	count = {} #keep track of occurrence
 	for i in range(0,len(a)):
 		sum_so_far += a[i]
 		sum_so_far = sum_so_far%K
@@ -35,5 +35,5 @@ def maxContiguousSum(a,K):
 a = [6, 6, 11, 15, 12, 1]
 K = 13
 max_sum,count = maxContiguousSum(a,K)
-print("input >>> %s max sum=%d #occurences=%d" % (a,max_sum,count))
+print("input >>> %s max sum=%d #occurrence=%d" % (a,max_sum,count))
 
