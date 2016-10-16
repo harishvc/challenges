c81497a602f3d27b74e1c83c7d130bfa51d64c40
Harish Chakravarthy <harishvc@gmail.com>
2016-10-15 20:51:02 -0700

    rewrite; simplify code :thumbsup:

--- list-find-product-no-division.py
+++ list-find-product-no-division.py
@@ -12,24 +12,21 @@ The trick is to construct a list (for 4 elements)
 
 
 #Find products without division
+#Time complexity: O(n)
 def findPWD(a):
-	size = len(a)
-	#result list initialized to 1
-	result = [1] * size
-	#go forward
-	tmp = 1
-	for i in range(0,size):
-		#IMPORTANT replace with tmp, since no prior value
-		result[i] = tmp
-		tmp = a[i]*tmp  #take value to next index
-	#go backward
-	tmp = 1
-	for i in range(size-1,-1,-1):
-		#IMPORTANT * tmp since values are already there
-		result[i] *= tmp 
-		tmp = tmp*a[i]   #take value to previous index
-	return result
-
+   size = len(a)
+   result = [1]*size
+   psf = a[0] #product so far
+   #go left to right
+   for i in range(1,size):
+      result[i] = psf
+      psf *= a[i]
+   psf = a[-1] #reset product so far with last value
+   #go right to left
+   for i in range(size-2, -1,-1):
+      result[i] *= psf
+      psf *= a[i]
+   return result
 
 a = [4,2,6]
 print(a, "  >>>> ", findPWD(a))
