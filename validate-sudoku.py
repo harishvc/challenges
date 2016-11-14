38ab3db28f9170ac99f7174d467e3c914a45118a
Harish Chakravarthy <harishvc@gmail.com>
2016-11-13 22:05:06 -0800

    major rewrite;fix logic :thumbsup:

--- validate-sudoku.py
+++ validate-sudoku.py
@@ -1,6 +1,7 @@
-'''
-Determine if a Sudoko puzzle is valid
+#Determine if a Sudoko puzzle is valid
+#https://leetcode.com/problems/valid-sudoku/
 
+'''
 Sudoku Rules:
 1. 9x9 grid
 2. Numbers 1-9 appear once in each row
@@ -12,21 +13,44 @@ Questions to ask:
 2. How are empty cells represented?
 3. Is an empty cell invalid Sudoku?
 '''
-#Reference: http://www.programcreek.com/2014/05/leetcode-valid-sudoku-java/
+
+
+'''
+NOTES
+
+Inspiration: 
+https://discuss.leetcode.com/topic/59807/python-short-code-with-explanation-with-small-space-used/3
+
+LOGIC:
+Store tuples in a set to keep track of visited values
+Reduces time complexity to nxn with additional storage (depending on values present)
+'''
+
 def ValidateSudoku(table):
-    #check rows for unique value
-    
-    #check cols for unique value
-    
-    
-    #check 3x3 for unique value
-    for block in range(9):
-        row = int(block/3) * 3
-        col = (block%3) * 3
-        for i in range(row,row+3):
-            for j in range(col,col+3):
-                #print("[%d,%d]" %(i,j))
-        #print("##########")
-                
+    rows = len(table)
+    cols = len(table[0])
+    visited = set()
+    for i in range(0,rows):
+        for j in range(0,cols):
+            if table[i][j] == "."
+                continue
+            else:
+                num = table[i][j]
+                #case 1: unique in row
+                if(("row",i,num) in res):
+                    return False
+                else:
+                    res.add(("row",i,num))
+                #case 2: unique in col
+                if(("col",j,num) in res):
+                    return False
+                else:
+                    res.add(("col",j,num))
+                #case 3: unique in 3x3
+                if((i//3,j//3,num) in res):
+                    return False
+                else:
+                    res.add((i//3,j//3,num))
+    return True
 
 ValidateSudoku([])
