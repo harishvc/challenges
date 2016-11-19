b5789ca34bd42c91744314420024939079bdda94
Harish Chakravarthy <harishvc@gmail.com>
2016-11-19 11:48:02 -0800

    major rewrite;clean-up;simplify code :cool:

--- dp-target-permutations.py
+++ dp-target-permutations.py
@@ -1,33 +1,20 @@
-#Given target and possible values find #permutations to achieve the target
-
-#Question: Find all possible paths
-#Solution: Similar to tree traversal 
-#Keep reducing the target value until the value == 0 or negative!
-def AllPermutations(target,values,path,level,result):
-	if target == 0:
-		#case 1: valid path, return 1
-		t = path[:level]
-		t.sort()
-		if t not in result:
-			result.append(t)
-		#IMPORTANT: return 1, valid path
-		return 1
-	elif target < 0:
-		#case 2: invalid path, return 0
-		return 0
-	else:
-		#case 3: continue to go further
-		count = 0
-		for d in values:
-			#IMPORTANT: insert current value to path at level
-			path.insert(level,d)
-			#IMPORTANT: level+1
-			#Add up all path where target==0
-			count += AllPermutations(target-d,values,path,level+1,result)
-		return count
+#Given target and possible values find 
+  #1. Total permutations to achieve the target
+  #2. All possible permutations
 
 
-def findPermutations(target,values):
+'''
+OBSERVATION:
+1. if we subtract possible values from target unit target ==0 the path is valid (valid permutation)
+                     6
+                 1/ 2\ 3\ 6\
+                 /    \  \  \
+                5     4  3   0  <- valid path (if target =0)
+2. If we start bottom up, we can compute #permutations from earlier computed values
+'''
+
+#Solution: Dynamic Programming (bottom up)
+def totalPermutations(target,values):
 	table = [0]*(target+1)
 	#IMPORTANT: table[0] = 0 , valid path!!!
 	table[0] = 1
@@ -38,16 +25,23 @@ def findPermutations(target,values):
 	#print(table)
 	return table[target]
 
-target = 12
+#Python generator
+def allPath(target,path,values):
+	if target == 0:
+		yield path
+	else:
+		for i in values:
+			if target >= i:
+				yield from allPath(target-i,path+[i],values)
+
+
+
+target = 6
 values = [1,2,3,6]
 result = []
 
-#Recursive solution
-#count = AllPermutations(target,values,[],0,result)
-#for r in result:
-#	print(r)
-#print(count)
+print("#Permutations to get target %d using values %s = %d" % (target,values, totalPermutations(target,values)))
 
-#DP Solution
-print("#Permutations to get target %d using values %s = %d" % (target,values, findPermutations(target,values)))
-
+print("\nall permutations")
+for i in allPath(target,[],values):
+	print(i)
