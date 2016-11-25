7aac390e6db4a2ea1a221d335599691f9ae38073
Harish Chakravarthy <harishvc@gmail.com>
2016-11-24 21:47:25 -0800

    add diagram;rewrite using yield :thumbsup:

--- binary-tree-root-to-leaf-paths-recursive.py
+++ binary-tree-root-to-leaf-paths-recursive.py
@@ -1,32 +1,39 @@
-'''
-Question: Print all paths & path sum from root to leaf (recursive)
-'''
+#Question: Print all paths & path sum from root to leaf
 
 import sys
 sys.path.append("./mylib")
 import Tree
- 
-#Modified post order traversal
-def Root2Leaf2(node,path,sum):
-    if node is None:
-        return
-    #step 1: update sum and path
+
+#Time complexity: O(n)
+#Space complexity: O(n)
+#Design: Iterate the binary tree level by level and store node, path and path sum in a stack
+def Root2LeafPaths(node,path=[],path_sum=0):
+    path_sum += node.data
     path.append(node.data)
-    sum += node.data    
-    #step 2: visit sub-tree on left
-    Root2Leaf2(node.left,path,sum)
-    #step 3: visit sub-tree on right
-    Root2Leaf2(node.right,path,sum)
-    #step 4: If you are here, you have visited all nodes in the left and right sub-tree!!!
-    #check if node is leaf?    
-    if(node.left is None and node.right is None):
-        print(path,sum)
-    #step 5: update sum and path (go up the tree to the parent node)
+    if node.left is None and node.right is None:
+        print("%s sum=%d" % (path,path_sum))
+    else:
+        Root2LeafPaths(node.left,path,path_sum) if node.left else None
+        Root2LeafPaths(node.right,path,path_sum) if node.right else None
     path.pop()
-    sum -= node.data
+    path_sum -= node.data
 
 
-     
+'''
+
+                         1
+                       /    \ 
+                      2       3
+                    /   \    /  \
+                  4      5  6    7
+                 /        \
+                8          9
+                            \
+                            10
+                             \
+                             11
+'''
+
 #Build binary tree 
 root = Tree.BinaryTree(1)
 root.insertLeft(2)
@@ -35,11 +42,9 @@ root.getLeft().insertLeft(4)
 root.getLeft().insertRight(5)
 root.getRight().insertLeft(6)
 root.getRight().insertRight(7)
+root.getLeft().getLeft().insertLeft(8)
+root.getLeft().getRight().insertRight(9)
+root.getLeft().getRight().getRight().insertRight(10)
+root.getLeft().getRight().getRight().getRight().insertRight(11)
 
-
-path = []
-print("root to leaf path & sum:")
-#Root2Leaf(root,path,0)
-#assert len(path) == 0, "Houston we have a problem!"
-Root2Leaf2(root,path,0)
-assert len(path) == 0, "Houston we have a problem!"
+Root2LeafPaths(root)
