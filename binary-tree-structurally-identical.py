771533af44954890a5f5c96b507ef93fa5a00573
Harish Chakravarthy <harishvc@gmail.com>
2016-11-28 22:45:09 -0800

    refactor code and add diagram

--- binary-tree-structurally-identical.py
+++ binary-tree-structurally-identical.py
@@ -1,5 +1,10 @@
 #Question: Given two binary trees, return true if they are structurally identical
 
+'''
+OBSERVATION:
+* left and right sub-trees are arranged in the exactly same way (no value comparison)
+'''
+
 import sys
 sys.path.append("./mylib")
 import Tree
@@ -11,25 +16,23 @@ def CheckIdentical(t1,t2):
 	#condition 2: one of the nodes is empty
 	elif( t1 is None or t2 is None):
 			return False
-	#condition 3: data does not match
-	elif( t1.getData() != t2.getData()):
-			return False
-	#condition 4: check for subnodes	
 	else:
 		return CheckIdentical(t1.getLeft(),t2.getLeft()) and  CheckIdentical(t1.getRight(),t2.getRight())
 
 
+'''
+           root                      root1
+               1                       1   
+             /   \                   /   \
+            2     3                 4     5
 
+'''
 root = Tree.BinaryTree(1)
 root.insertLeft(2)
 root.insertRight(3)
-root.getLeft().insertLeft(4)
-root.getLeft().insertRight(5)
-root.getRight().insertLeft(6)
 
 root1 = Tree.BinaryTree(1)
-root1.insertLeft(2)
-root1.insertRight(3)
+root1.insertLeft(4)
+root1.insertRight(5)
 
-print ("(root,root) structurally identical? %s" % (CheckIdentical(root,root)))
 print ("(root,root1) structurally identical? %s" % (CheckIdentical(root,root1)))
