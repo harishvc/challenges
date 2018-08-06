7020a9225a8486cdbef03d4eb9afc8086db2559b
Harish Chakravarthy <harishvc@gmail.com>
2018-08-05 20:35:25 -0700

    add solution #2 - simplified

--- binary-search-tree-largestK.py
+++ binary-search-tree-largestK.py
@@ -13,6 +13,7 @@ for i in range(1,len(a)):
         BST.insert(root,node)
 
 #Reverse in order traversal to get values in descending order
+#solution #1
 def largestK(node,K):
     if node and K > 0:
         K = largestK(node.right,K)
@@ -26,11 +27,26 @@ def largestK(node,K):
         #Important: return K
         return K
 
+#solution #2
+def largek(node,results,k):
+    if node and len(results) <k:
+        if node.right:
+            largek(node.right,results,k)
+        results.append(node.data)
+        largek(node.left,results,k)
 
 print("In order traversal >>")
 BinaryTreeTraversal.inOrder(root)
 print("")
 
-k= 4
-print("Largest ", k)
-largestK(root,k)
+
+k= 2
+#solution #1
+print(k, "largest:")
+# largestK(root,k)
+
+#solution #2
+results = []
+size = 0
+largek(root,results,k)
+print(results[k-1])
