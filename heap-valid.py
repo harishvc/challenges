7dbcc22a12c487c5d1fa1a788b47908ee033fc41
Harish Chakravarthy <harishvc@gmail.com>
2018-08-11 22:14:03 -0700

    add: valid complete binary tree

---
+++ heap-valid.py
@@ -0,0 +1,7 @@
+'''
+Question: Check if a Binary tree is a Complete Binary Tree?
+
+Reference:
+1. https://www.geeksforgeeks.org/check-whether-binary-tree-complete-not-set-2-recursive-solution/
+
+'''
