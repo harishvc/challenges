d465d6a3f66eaaf39822f45186dd85d2c0c5b215
Harish Chakravarthy <harishvc@gmail.com>
2018-08-19 00:44:40 -0700

    modify logic + add :notes:

--- dp-string-combinations.py
+++ dp-string-combinations.py
@@ -1,46 +1,74 @@
-#Print string combinations on length K
+#Find all string combinations
 
 '''
 NOTES:
-Combination can be considered as a COMPLETE GRAPH TRAVERSAL problem without 
-repeating characters in a different order - stop recursing branches with repeated characters.
+1. In combinations order does not matter. Example: lottery ticket 
+2. Combinations can have strings of different length including empty one!
+3. Combination can be considered as a COMPLETE GRAPH TRAVERSAL problem without visiting any node twice
+
+input = abc (n=3)
+- formula = n!/(n-r)! * r! 
+- part a: when length = 1 (r=1 and n = 3). #combinations = 3
+- part b: when length = 2 (r=2 and n = 3). #combinations = 3
+- part c: when length = 3 (r=3 and n = 3). #combinations = 1
+- part d: empty set []
+- total combinations = 3 + 3 + 1 + 1 = 8
+
 '''
 
-#Solution: Graph traversal 
-'''
-1. Traversing a COMPLETE GRAPH
-2. LIMITATION:
-   - Does not handle duplicates
-   - Repeating values
 
-REFERENCE:
-1. http://exceptional-code.blogspot.com/2012/09/generating-all-permutations.html
-'''
 
-def findCombinations(a,k,start,count,path,result):
-	if k == count:
-		result.append(path[0:k])
-		return
-	for i in range(start,len(a)):
-		#Avoid duplicate values
-		if count >  0 and path[count-1] == a[i]:
-			continue
-		path.insert(count,a[i])
-		findCombinations(a,k,start+1,count+1,path,result)
 
-def mysort(self):
-	self.sort()
-	return self
+def pre_processing(linput):
+    #step 1: find unique characters and count
+    #c_dict = { 'A' : 2,
+    #            'B' : 1,
+    #            'C' : 1 }
+    c_dict = {} 
+    for key in linput:
+        if key in c_dict:
+            c_dict[key] += 1
+        else:
+            c_dict[key] = 1
 
-a = "abc"  
-k = 2
-result = []
-print("Combinations of %s of size=%s >>>" % (a,k))
-findCombinations(a,k,0,0,[],result)
+    #step2: Sort unique characters and create two list - unique characters and count
+    #characters = ['A','B','C']
+    #character_count = [2,1,1]
+    characters = []
+    character_count = []
+    for key in sorted(c_dict):
+        characters.append(key)
+        character_count.append(c_dict[key])
 
-#Remove duplicates
-#1: Sort
-result.sort(key=mysort)
-#2. convert to tuple and put them in a set
-s = set(map(tuple,result))
-print(s)
+    return characters,character_count 
+
+
+def find_combinations(characters,character_count,level,path,unique_combinations):
+    #new combinations!
+    #print(path[0:level])
+    t = path[0:level]  
+    t.sort()
+    t2 = "".join(t)
+    #add to set - retain only unique values
+    unique_combinations.add(t2)
+
+    for i in range(0,len(characters)):
+        #check count
+        if character_count[i] == 0:
+            continue
+        else:
+            path[level] = characters[i]
+            character_count[i] = character_count[i] - 1
+            level = level + 1
+            find_combinations(characters, character_count, level, path,unique_combinations)
+            level = level - 1
+            character_count[i] = character_count[i] + 1
+
+
+linput = ['B','A','C']
+characters, character_count = pre_processing(linput)
+result = [0 for i in range(len(linput))]
+unique_combinations = set()
+print ("Combinations of %s" % ("".join(linput)))
+find_combinations(characters,character_count,0,result,unique_combinations)
+print(unique_combinations)
