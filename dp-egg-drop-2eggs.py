c6bea6ec65a00f7770371ba16f941c0cbbeb8f76
Harish Chakravarthy <harishvc@gmail.com>
2018-09-20 22:33:00 -0700

    update comments

--- dp-egg-drop-2eggs.py
+++ dp-egg-drop-2eggs.py
@@ -1,44 +1,43 @@
-#Given some number of floors and 2 eggs, what is the minimum number 
+'''
+Given some number of floors and 2 eggs, what is the minimum number 
 #of attempts it will take to find out from which floor egg will break in the worst case.
 
-'''
-REFERENCE:
+REFERENCES:
 1. https://www.interviewcake.com/question/python/two-egg-problem
 2. https://www.mathsisfun.com/algebra/quadratic-equation.html
 3. https://github.com/harishvc/challenges/blob/master/images/egg-drop-recursion.jpg
+4. https://www.interviewcake.com/concept/java/triangular-series
+'''
 
-
+'''
+ANALYSIS:
 Solution 1: Apply Binary search concept
    -- By reducing the problem size by half in each iteration.
    -- if we start at floors//2 . We have two choices, the egg will break or not
       -- if egg breaks go down
       -- if egg does not break go up
-   -- if the egg breaks at floor 50 , max attemps for worst case is 49
-   -- can do better than that!
+   -- if the egg breaks at floor 99 , attempts for worst case is 50 (1+49)
+   -- can do better than that?
 
-Solution 2: Escape floors by a constant value? If so what is an optimal value?
-    -- Let's say we break into small groups of 5 . Drop egg on floor 5
-        -- if egg breaks, max attempts for worst case is 4
-        -- if does not break, we do next 5 , next 5 ,,,,,, 20 times!
-        -- max attemps for worst case is 20
-    -- Can we change the smaller group from 5 to 10?
-        -- if egg breaks at floor 10, max attempts for worst case is 9
-        -- if egg break at floor 21 .... , max attemps will be 12 
-           1(10),2(20),3(30,breaks!),4 (29),5(28),6(27),7(26),8(25),9(24),10(23),11(22),12(21)
-        -- if egg break at floor 31 .... , max attemps will be 13
-           1(10),2(20),3(30),4(40,breaks!),5(39),6(38),7(37),8(36),9(35),10(34),11(33),12(32),13(31)
-        -- max attempts INCREASES as we move up
+Solution 2: Skip floors by a constant value? If so what is an optimal value?
+    -- Break into small groups of 5 
+        -- if the egg breaks at floor 99, attempts for worst case is 23 (19+4)
+    -- Break into smaller group of 10
+        -- if the egg breaks at floor 99, attempts for worst case is 19 (10+9)
 
-Solution 3: Can we escape few floors each time such that max attempts is CONSTANT?
-	-- Let's say if max attemps is 10
-	    -- start at floor 10
-		-- next iteration we start at floor 19 (not 20)
-		-- following iteration we can then start at floor 28 (not 30)
-		-- #floors we can escape is n + n-1 + n-2  (== total floors)
-		-- n +  n-1 + n-2 ..... n(n+1)/2 
-		n^2 + n = 200       //if total floors == 100
-		n^2 + n - 200 = 0   //QUADRATIC equation to solve for n
-		n =  -b +- sqrt(b^2 - 4ac) /2a   here a = 1, b =1 , c= -200
+Solution 3: Skip floors such that attempts for worst case is CONSTANT?
+	-- Let's say if max attempts is 10
+     -- if the egg breaks at floor 10, attempts for worst case is 10 (1+9), floors we can skip = 10
+     -- if the egg breaks at floor 19, attempts for worst case is 10 (2+8), floors we can skip = 9
+     -- if the egg breaks at floor 27, attempts for worst case is 10 (3+7), floors we can skip = 8
+     -- #floors we can skip is n + n-1 + n-2  = total_number_of_floors (100)
+        10 + 9 + 8 + 7 + ...... + 1 = 100   
+        n +  n-1 + n-2 ..... +1 = n(n+1)/2 //TRIANGULAR series (starts with 1 and increases by 1)
+        n(n+1)/2 = 100 
+		    n^2 + n  = 200       
+		    n^2 + n - 200 = 0   //QUADRATIC equation to solve for n
+		    n =  -b +- sqrt(b^2 - 4ac) /2a   here a = 1, b =1 , c= -200
+    -- By finding number of floors to skip (n)  you are also finding the min attempts that handles worst case    
 '''
 
 import math
