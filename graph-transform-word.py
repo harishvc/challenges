ff0decc9b04ef4755222c7727603c4d873c7064c
Harish Chakravarthy <harishvc@gmail.com>
2018-09-22 23:33:11 -0700

    add comments :notes:

--- graph-transform-word.py
+++ graph-transform-word.py
@@ -1,38 +1,63 @@
 '''
-Convert string a to b using a dictionary of words
+Convert string a to b using a dictionary of words  #WordLadder
 
 NOTES:
 1. Create a adjacency list
 2. BFS traversal will find the shortest path
 
-PYTHON3
-#characters a-z
-s = list(map(chr,range(ord('a'),ord('z')+1)))
+>>> ord('a')
+97
+>>> ord('z')
+122
+>>> chr(97)
+'a'
 '''
 
-#Time Complexity: O(nm^2). 
-#n = # words, 
-#m = length of word in dictionary - Pre-processing 
+
+# Time Complexity: O(l*(w^2))
+# w = # words in the dictionary
+# l = sum of length of words in dictionary
+#
+#
+# Space complexity: O(w + e)
+# w = # words in the dictionary
+# e = sum of edges
 def generateAdjacencyList(dictionary):
 	graph = {}
-	az = list(map(chr,range(ord('a'),ord('z')+1)))
+	az= [chr(index) for index in range(ord('a'),ord('z')+1)]
 	for word in dictionary:
+		size = len(word)
 		graph[word] = set()
-		for i in range(len(word)):
-			#Remove 1 character
-			if word[:i]+word[i+1:] in dictionary:
-				graph[word].add(word[:i]+word[i+1:])
-			#Replace 1 chanacter
+		
+		#remove 1 character
+		#cat => at , ct , at
+		#bat => at, bt, ba
+		for index in range(0,size):
+			if word[:index]+word[index+1:] in dictionary:
+				graph[word].add(word[:index]+word[index+1:])
+
+		#replace 1 character
+		#cat => aat, bat ........ zat
+		#    => cat, cbt  ........czt
+		#
+		#bat => aat, bat ......... zat
+		# 
+		for index in range(0,size):
 			for c in az:
-				if word[:i]+c+word[i+1:] in dictionary:
-					graph[word].add(word[:i]+c+word[i+1:])
-			#Add 1 character
+				if word[:index]+c+word[index+1:] in dictionary:
+					graph[word].add(word[:index]+c+word[index+1:])		
+
+		#add 1 character
+		#cat => acat, bcat ....... zcat
+		#    =>	caat, cbat ........czat
+		for index in range(0,size):
 			for c in az:
-				if word[:i+1]+c+word[i+1:] in dictionary:
-					graph[word].add(word[:i+1]+c+word[i+1:])
-	#for key in graph:
-	#	print(key, graph[key])
+				if c+word[:index+1]+word[index:] in dictionary:
+					graph[word].add(c+word[:index+1]+word[index:])
 	return graph
+
+
+from graph-traversal-bfs import BFT
 
 import queue
 def BFSTraversal(graph,start,end):
@@ -53,7 +78,10 @@ def BFSTraversal(graph,start,end):
 	return None
 
 dictionary = ["cat", "bat", "hat", "bad", "had"]
+#pre-processing
 graph = generateAdjacencyList(dictionary)
 start= "bat"
 end = "had"
 print("Transforming %s to %s >> %s" % (start,end,BFSTraversal(graph,start,end)))
+
+BFT(graph,start,end)
